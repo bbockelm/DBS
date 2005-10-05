864fed2bdc22a45e0c433a4018f3d6551947dc44
lat <>
2005-10-05 07:27:40 +0000

    Replace long analysis_dataset or previous dataset with anads.  Shorten t_event_collection_run table name and give it constraints

--- Schema/RefDBLegacy/OracleExtra.sql
+++ Schema/RefDBLegacy/OracleExtra.sql
@@ -1,13 +1,13 @@
 -- ======================================================================
 create sequence seq_run;
-create sequence seq_info_dataset;
+create sequence seq_info_anads;
 create sequence seq_info_evcoll;
-create sequence seq_event_collection_run;
+create sequence seq_evcoll_run;
 create sequence seq_event_collection_ana_data;
 
 -- ======================================================================
 
-create table t_info_dataset
+create table t_info_anads
   (analysis_dataset		integer		not null,
    events			integer		not null,
    estimated_luminosity		varchar (80),
@@ -40,35 +40,35 @@ create table t_run
    modified_at			float		not null,
    modified_by			integer		not null);
 
-create table t_event_collection_run
+create table t_evcoll_run
   (event_collection		integer		not null,
    run				integer		not null);
 
 -- ======================================================================
-alter table t_info_dataset
-  add constraint pk_info_dataset
+alter table t_info_anads
+  add constraint pk_info_anads
   primary key (analysis_dataset)
   using index tablespace CMS_DBS_INDX01;
 
-alter table t_info_dataset
-  add constraint fk_info_dataset_ds
+alter table t_info_anads
+  add constraint fk_info_anads_ds
   foreign key (analysis_dataset) references t_analysis_dataset (id)
   /* on upfloat cascade */ on delete cascade;
 
-alter table t_info_dataset
-  add constraint fk_info_dataset_status
+alter table t_info_anads
+  add constraint fk_info_anads_status
   foreign key (status) references t_dataset_status (id);
 
-alter table t_info_dataset
-  add constraint fk_info_dataset_valid
+alter table t_info_anads
+  add constraint fk_info_anads_valid
   foreign key (validation_status) references t_validation_status (id);
 
-alter table t_info_dataset
-  add constraint fk_info_dataset_creat
+alter table t_info_anads
+  add constraint fk_info_anads_creat
   foreign key (created_by) references t_person (id);
 
-alter table t_info_dataset
-  add constraint fk_info_dataset_mod
+alter table t_info_anads
+  add constraint fk_info_anads_mod
   foreign key (modified_by) references t_person (id);
 
 --
@@ -120,23 +120,36 @@ alter table t_run
   add constraint fk_run_mod
   foreign key (modified_by) references t_person (id);
 
--- ======================================================================
 --
+alter table t_evcoll_run
+  add constraint pk_evcoll_run
+  primary key (event_collection, run)
+  using index tablespace CMS_DBS_INDX01;
 
-create index ix_info_dataset_status
-  on t_info_dataset (status)
+alter table t_evcoll_run
+  add constraint fk_evcoll_run_evcoll
+  foreign key (event_collection) references t_event_collection (id);
+
+alter table t_evcoll_run
+  add constraint fk_evcoll_run_run
+  foreign key (run) references t_run (id);
+
+-- ======================================================================
+
+create index ix_info_anads_status
+  on t_info_anads (status)
   tablespace CMS_DBS_INDX01;
 
-create index ix_info_dataset_valid
-  on t_info_dataset (validation_status)
+create index ix_info_anads_valid
+  on t_info_anads (validation_status)
   tablespace CMS_DBS_INDX01;
 
-create index ix_info_dataset_creat
-  on t_info_dataset (created_by)
+create index ix_info_anads_creat
+  on t_info_anads (created_by)
   tablespace CMS_DBS_INDX01;
 
-create index ix_info_dataset_mod
-  on t_info_dataset (modified_by)
+create index ix_info_anads_mod
+  on t_info_anads (modified_by)
   tablespace CMS_DBS_INDX01;
 
 --
