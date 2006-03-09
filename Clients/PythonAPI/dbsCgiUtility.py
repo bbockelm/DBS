580f6dec66ed2ad35f63ebba85ce2c7539e9d167
lat <>
2006-03-09 15:25:55 +0000

    Clean up and simplify CGI API code considerably.  Implement 'listDatasets'. 'Know' the best URL to execute queries by default, clients should override the URL only if they really do know better.

--- Clients/PythonAPI/dbsCgiUtility.py
+++ Clients/PythonAPI/dbsCgiUtility.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# $Id: dbsCgiUtility.py,v 1.11 2006/01/28 19:40:26 afanfani Exp $
+# $Id: dbsCgiUtility.py,v 1.12 2006/03/08 13:49:43 lat Exp $
 #
 # Class which uses CGI utilities to extract info from the db.
 #
@@ -8,20 +8,17 @@
 import os
 import string
 import urllib2
+from xml.dom.minidom import parseString
+from xml.parsers.expat import ExpatError
 
 import dbsException
 import dbsStaticMethod
 import dbsApi
-import dbsDatasetContentsXmlParser
-import dbsDatasetProvenanceXmlParser
 import dbsLogManager
-import dbsDataset
-
-CGI_DBS_XML_DUMP_SCRIPT_ = "dbsxml"
-
-DBS_STATUS_CODE_TAG_ = "Dbs-status-code"
-DBS_STATUS_MESSAGE_TAG_ = "Dbs-status-message"
-DBS_STATUS_DETAIL_TAG_ = "Dbs-status-detail"
+from dbsDataset import *
+from dbsFileBlock import *
+from dbsEventCollection import *
+from dbsLogManager import *
 
 # CGI status codes.
 CGI_SUCCESS_CODE_ = 100
@@ -32,48 +29,54 @@ CGI_GENERIC_FAILURE_CODE_ = 400
 # DBS Cgi utility class exceptions.
 
 class DbsCgiUtilityException(dbsException.DbsException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     dbsException.DbsException.__init__(self, **kwargs)
 
 class InvalidCgiScriptUrl(DbsCgiUtilityException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     DbsCgiUtilityException.__init__(self, **kwargs)
 
-
 class CgiToolError(DbsCgiUtilityException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     DbsCgiUtilityException.__init__(self, **kwargs)
 
 class InvalidXML(DbsCgiUtilityException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     DbsCgiUtilityException.__init__(self, **kwargs)
 
 class InvalidDatasetPathName(DbsCgiUtilityException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     DbsCgiUtilityException.__init__(self, **kwargs)
 
 class InvalidDataTier(DbsCgiUtilityException):
-
   def __init__ (self, **kwargs):
     """ Initialization. """
     DbsCgiUtilityException.__init__(self, **kwargs)
 
-  
+class DbsXmlParserException(dbsException.DbsException):
+  def __init__ (self, **kwargs):
+    """ Initialization. """
+    dbsException.DbsException.__init__(self, **kwargs)
+
+class InvalidInputXmlFile(DbsXmlParserException):
+  def __init__ (self, **kwargs):
+    """ Initialization. """
+    DbsXmlParserException.__init__(self, **kwargs)
+
+class InvalidXmlInput(DbsXmlParserException):
+  def __init__ (self, **kwargs):
+    """ Initialization. """
+    DbsXmlParserException.__init__(self, **kwargs)
+
 ##############################################################################
 # HTTP exception mapper.
 
 class DbsCgiExceptionMapper:
-
   __exceptionMap = {
     CGI_INVALID_DATA_TIER_CODE_ : "InvalidDataTier",
     CGI_GENERIC_FAILURE_CODE_ : "CgiToolError",
@@ -86,187 +89,172 @@ class DbsCgiExceptionMapper:
 
   getExceptionClassName = dbsStaticMethod.DbsStaticMethod(getExceptionClassName)
 
-  
+##############################################################################
+# Utility functions.
+
+def dbsCgiError (log, code, message, reason, context, chain):
+  exclass = DbsCgiExceptionMapper.getExceptionClassName (code)
+  log.log ("Raising %s due to %s" % (exclass, reason), context, LOG_LEVEL_ERROR_)
+  exec "raise %s (exception = chain, args = \"\"\"%s\"\"\")" % (exclass, message)
+
+def dbsCgiCall (context, log, url):
+  try:
+    # Announce intent
+    log.log("Starting CGI call to %s" % url, LOG_LEVEL_INFO_)
+
+    # Fetch result from the CGI server
+    result = urllib2.urlopen (url)
+    data = result.read ();
+    statusCode = int(result.headers.get ('Dbs-status-code'))
+    statusMessage = result.headers.get ('Dbs-status-message')
+    statusDetail = result.headers.get ('Dbs-status-detail')
+
+    # Log result
+    log.log("CGI call completed, status code: %s (status message: '%s', status detail: '%s')."
+	% (statusCode, statusMessage, statusDetail), context, LOG_LEVEL_INFO_)
+
+    # If there was a server-side error, raise an appropriate exception
+    if statusCode != 100:
+      exmsg = "Status message: '%s', Status detail: '%s'" % (statusMessage, statusDetail)
+      dbsCgiError (log, statusCode, exmsg, "server error", context, None)
+
+    # All is ok, return the data
+    return data
+
+  except urllib2.URLError, ex:
+    # URL access failed, raise an exception
+    dbsCgiError (log, ex.code, ex, "URL access error", context, ex)
+
+def dbsCgiXmlParse (context, log, xmlString):
+  """ Constructor. """
+  if xmlString == None:
+    errmsg = "Tried to parse non-existent XML"
+    log.log (errmsg, context, LOG_LEVEL_ERROR_)
+    raise InvalidXmlInput(args=errmsg)
+
+  try:
+    dom = parseString(xmlString)
+    log.log("XML parsed successfully", context, LOG_LEVEL_DEBUG_)
+    return dom
+  except ExpatError, ex:
+    log.log("XML failed to parse: %s (input %s)" % (ex, xmlString), context, LOG_LEVEL_ERROR_)
+    raise InvalidXmlInput(exception=ex)
+  except Exception, ex:
+    log.log("XML failed to parse: %s" % ex, context, LOG_LEVEL_ERROR_)
+    raise DbsXmlParserException(exception=ex)
+
 ##############################################################################
 # CGI utility class.
 
 class DbsCgiUtility:
-
   def __init__(self, cgiUrl=None):
     """ Constructor. """
     self._cgiUrl = cgiUrl
-    self._logManager = dbsLogManager.getInstance()
+    if cgiUrl is None:
+      self._cgiUrl = "http://cern.ch/cms-dbs/cgi-bin/dbsxml"
+      self._cgiUrl = "http://cmsdoc.cern.ch/cms/aprom/DBS/CGIServer/dbsxml"
+    self._log = dbsLogManager.getInstance()
     
   def setCgiUrl(self, cgiUrl):
     """ Set cgi script url. """
     self._cgiUrl = cgiUrl
 
+  def listDatasets(self, pattern):
+    """ Retrieve datasets matching a pattern string. """
+    funcName = "%s.%s" % (self.__class__.__name__, "listDatasets()")
+
+    # Invoke cgi script.
+    url = "%s?api=listDatasets&pattern=%s" % (self._cgiUrl, pattern)
+    data = dbsCgiCall (funcName, self._log, url)
+
+    # Parse the resulting xml output.
+    self._log.log("Parsing CGI result", funcName, LOG_LEVEL_DEBUG_)
+    try:
+      datasetList = []
+      dom = dbsCgiXmlParse (funcName, self._log, data);
+      for element in dom.getElementsByTagName ('processed-dataset'):
+	path = element.getAttribute('path').encode ('ascii')
+	datasetList.append (DbsDataset (datasetPath = path))
+      return datasetList
+    except Exception, ex:
+      raise InvalidXML(exception=ex)
+
   def getDatasetContents(self, datasetPathName):
     """ Retrieve event collections given the dataset path name string. """
-
     funcName = "%s.%s" % (self.__class__.__name__, "getDatasetContents()")
 
     # Check path.
-    try:
-      dbsDataset.DbsDataset.verifyDatasetPathName(datasetPathName)
-    except dbsDataset.InvalidDatasetPathName, ex:
-      raise InvalidDatasetPathName(args="%s" % ex)
-    
-    # Construct cgi path.
-    cgiUrl = "%s/%s?api=getDatasetContents&path=%s" % (
-      self._cgiUrl, CGI_DBS_XML_DUMP_SCRIPT_, datasetPathName)
+    DbsDataset.verifyDatasetPathName(datasetPathName)
 
     # Invoke cgi script.
-    try:
-      self._logManager.log(
-	what="Retrieving dataset contents, cgiUrl: %s." % cgiUrl,
-	where=funcName,
-	logLevel=dbsLogManager.LOG_LEVEL_INFO_)
-      urlResult = urllib2.urlopen(cgiUrl)
-      xmlString = urlResult.read() 
-      dbsStatusCode = int(urlResult.headers.get(DBS_STATUS_CODE_TAG_))
-      dbsStatusMessage = urlResult.headers.get(DBS_STATUS_MESSAGE_TAG_)
-      dbsStatusDetail = urlResult.headers.get(DBS_STATUS_DETAIL_TAG_)
-      self._logManager.log(
-	what="CGI call completed, status code: %s (status message: '%s', status detail: '%s')." % (dbsStatusCode, dbsStatusMessage, dbsStatusDetail),
-	where=funcName,
-	logLevel=dbsLogManager.LOG_LEVEL_INFO_)
-      if dbsStatusCode != CGI_SUCCESS_CODE_:
-	# Cgi failed for some reason, determine exception to be raised.
-	cgiExClassName = DbsCgiExceptionMapper.getExceptionClassName(dbsStatusCode)
-	cgiExArgs = "Status message: '%s', Status detail: '%s'" % (dbsStatusMessage, dbsStatusDetail)
-	exec "cgiEx = %s(args=\"\"\"%s\"\"\")" % (cgiExClassName, cgiExArgs)
-	errMsg = "CGI Error caught, will raise %s" % (cgiEx.__class__.__name__)
-	self._logManager.log(what=errMsg,
-			     where=funcName,
-			     logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-	raise cgiEx
-    except DbsCgiUtilityException, ex:
-      raise
-    except urllib2.URLError, ex:
-      # Cgi failed for some reason, determine exception to be raised.
-      cgiExClassName = DbsCgiExceptionMapper.getExceptionClassName(ex.code)
-      exec "cgiEx = %s(exception=ex, args=\"\"\"%s (Input argument datasetPathName=\"%s\")\"\"\")" % (cgiExClassName, ex.read(), datasetPathName)
-      errMsg = "URLError caught: \n%s\nWill raise %s" % (
-	ex, cgiEx.__class__.__name__)
-      self._logManager.log(what=errMsg,
-			   where=funcName,
-			   logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-      raise cgiEx
-    except Exception, ex:
-      # General exception.
-      self._logManager.log(what="Cgi tool failed.\n%s " % ex,
-			   where=funcName,
-			   logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-      raise CgiToolError(exception=ex)
-    self._logManager.log(
-      what="Retrieved dataset contents.",
-      where=funcName,
-      logLevel=dbsLogManager.LOG_LEVEL_DEBUG_)
+    url = "%s?api=getDatasetContents&path=%s" % (self._cgiUrl, datasetPathName)
+    data = dbsCgiCall (funcName, self._log, url)
 
-    # Parse the resulting xml output.
-    self._logManager.log(
-      what="Parsing XML CGI output string.",
-      where=funcName,
-      logLevel=dbsLogManager.LOG_LEVEL_DEBUG_)
+    # Parse the resulting xml output.  The output consits of a list of blocks,
+    # each with its list of event collections.
+    self._log.log("Parsing CGI result", funcName, LOG_LEVEL_DEBUG_)
     try:
-      datasetContentsParser = dbsDatasetContentsXmlParser.DbsDatasetContentsXmlParser(xmlString=xmlString)
-      fileBlockList = datasetContentsParser.getFileBlockList()
+      fileBlockMap = {}
+      dom = dbsCgiXmlParse (funcName, self._log, data)
+      # Parse blocks
+      for element in dom.getElementsByTagName ('block'):
+	name = element.getAttribute('name').encode('ascii')
+	id = int(element.getAttribute('id').encode('ascii'))
+        if not fileBlockMap.has_key (id):
+	  fileBlockMap [id] = DbsFileBlock (blockName = name, blockId = id)
+	self._log.log ("Found file block %s (id %d)" % (name, id), funcName, LOG_LEVEL_DEBUG_)
+
+      # Parse event collections and associated with blocks
+      for element in dom.getElementsByTagName ('event-collection'):
+	name = element.getAttribute ('name').encode ('ascii')
+	events = int(element.getAttribute ('events'))
+	inblock = int(element.parentNode.getAttribute ('id'))
+	fileBlockMap [inblock].addEventCollection (
+	  DbsEventCollection (collectionName = name, numberOfEvents = events))
+
+      return fileBlockMap.values ()
     except Exception, ex:
       raise InvalidXML(exception=ex)
-      
-    return fileBlockList
 
   def getDatasetProvenance(self, datasetPathName, dataTierList):
     """ Retrieve dataset parents given the dataset path name string. """
-
     funcName = "%s.%s" % (self.__class__.__name__, "getDatasetProvenance()")
 
     # Check path.
-    try:
-      dbsDataset.DbsDataset.verifyDatasetPathName(datasetPathName)
-    except dbsDataset.InvalidDatasetPathName, ex:
-      raise InvalidDatasetPathName(args="%s" % ex)
-    
-    # Construct cgi path.
-    cgiUrl = "%s/%s?api=getDatasetProvenance&path=%s" % (
-      self._cgiUrl, CGI_DBS_XML_DUMP_SCRIPT_, datasetPathName)
-    if len(dataTierList):
-      cgiUrl = "%s&datatier=%s" % (cgiUrl, string.join(dataTierList, ","))
-    
+    DbsDataset.verifyDatasetPathName(datasetPathName)
+
     # Invoke cgi script.
-    try:
-      self._logManager.log(
-	what="Retrieving dataset provenance, cgiUrl: %s." % cgiUrl,
-	where=funcName,
-	logLevel=dbsLogManager.LOG_LEVEL_INFO_)
-      urlResult = urllib2.urlopen(cgiUrl)
-      xmlString = urlResult.read() 
-      dbsStatusCode = int(urlResult.headers.get(DBS_STATUS_CODE_TAG_))
-      dbsStatusMessage = urlResult.headers.get(DBS_STATUS_MESSAGE_TAG_)
-      dbsStatusDetail = urlResult.headers.get(DBS_STATUS_DETAIL_TAG_)
-      self._logManager.log(
-	what="CGI call completed, status code: %s (status message: '%s', status detail: '%s')." % (dbsStatusCode, dbsStatusMessage, dbsStatusDetail),
-	where=funcName,
-	logLevel=dbsLogManager.LOG_LEVEL_INFO_)
-      if dbsStatusCode != CGI_SUCCESS_CODE_:
-	# Cgi failed for some reason, determine exception to be raised.
-	cgiExClassName = DbsCgiExceptionMapper.getExceptionClassName(dbsStatusCode)
-	cgiExArgs = "Status message: '%s', Status detail: '%s'" % (dbsStatusMessage, dbsStatusDetail)
-	exec "cgiEx = %s(args=\"\"\"%s\"\"\")" % (cgiExClassName, cgiExArgs)
-	errMsg = "CGI Error caught, will raise %s" % (cgiEx.__class__.__name__)
-	self._logManager.log(what=errMsg,
-			     where=funcName,
-			     logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-	raise cgiEx
-    except DbsCgiUtilityException, ex:
-      raise
-    except urllib2.URLError, ex:
-      # Cgi failed for some reason, determine exception to be raised.
-      cgiExClassName = DbsCgiExceptionMapper.getExceptionClassName(ex.code)
-      exec "cgiEx = %s(exception=ex, args=\"\"\"%s (Input argument datasetPathName=\"%s\")\"\"\")" % (cgiExClassName, ex.read(), datasetPathName)
-      errMsg = "URLError caught: \n%s\nWill raise %s" % (
-	ex, cgiEx.__class__.__name__)
-      self._logManager.log(what=errMsg,
-			   where=funcName,
-			   logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-      raise cgiEx
-    except Exception, ex:
-      # General exception.
-      self._logManager.log(what="Cgi tool failed.\n%s " % ex,
-			   where=funcName,
-			   logLevel=dbsLogManager.LOG_LEVEL_ERROR_)
-      raise CgiToolError(exception=ex)
-    self._logManager.log(
-      what="Retrieved dataset provenance.",
-      where=funcName,
-      logLevel=dbsLogManager.LOG_LEVEL_DEBUG_)
+    url = "%s?api=getDatasetProvenance&path=%s" % (self._cgiUrl, datasetPathName)
+    if len(dataTierList):
+      url = "%s&datatier=%s" % (url, string.join(dataTierList, ","))
+    data = dbsCgiCall (funcName, self._log, url)
 
-    
     # Parse the resulting xml output.
-    self._logManager.log(
-      what="Parsing XML CGI output string.",
-      where=funcName,
-      logLevel=dbsLogManager.LOG_LEVEL_DEBUG_)
-
+    self._log.log("Parsing CGI result", funcName, LOG_LEVEL_DEBUG_)
     try:
-      datasetProvenanceParser = dbsDatasetProvenanceXmlParser.DbsDatasetProvenanceXmlParser(xmlString=xmlString)
-      datasetParentList = datasetProvenanceParser.getDatasetParentList()
+      parentList = []
+      dom = dbsCgiXmlParse (funcName, self._log, data)
+      for element in dom.getElementsByTagName ('parent'):
+	path = element.getAttribute('path').encode('ascii')
+	tier = element.getAttribute('tier').encode('ascii')
+	type = element.getAttribute('type').encode('ascii')
+	parentList.append (DbsDataset (datasetPath = path,
+				       dataTier = tier,
+				       dataType = type))
+      return parentList
     except Exception, ex:
       raise InvalidXML(exception=ex)
-      
-    return datasetParentList
 
 ##############################################################################
 # Unit testing.
 
 if __name__ == "__main__":
   try:
-    logLevel = dbsLogManager.LOG_LEVEL_ALL_
+    logLevel = LOG_LEVEL_ALL_
     dbsLogManager.getInstance().setLogLevel(logLevel)
 
     datasetPath = "/bt03_B0sJPsiX/Hit/bt_Hit245_2_g133"
-    cgiUtility = DbsCgiUtility(cgiUrl="http://cmsdoc.cern.ch/cms/aprom/DBS/CGIServer")
+    cgiUtility = DbsCgiUtility()
 
     
     print "Getting dataset contents for: %s" % datasetPath
