5a44cb8ac21b6e2ed92f8642f660411f7a534623
sekhri <>
2006-04-21 16:46:07 +0000

    Setup.sh is modified

--- Servers/AppServer/setup.sh
+++ Servers/AppServer/setup.sh
@@ -1,17 +1,17 @@
-export LOG4CXX=/data/Depend//log4cxx
-export ODBCHOME=/data/Depend//unixODBC
-export ORACLE_ODBC_HOME=/data/Depend//oracleodbcinstall
-export GSOAPHOME=/data/Depend//gSOAP
-export SWIG_HOME=/data/Depend//swig
-export WSDL2PYHOME=/data/Depend//wsdl2py
+export LOG4CXX=/home/sekhri/apps/log4cxx
+export ODBCHOME=/home/sekhri/apps/unixodbc/
+#export ORACLE_ODBC_HOME=/home/sekhri/apps/oracle/oracle_odbc_driver
+export ORACLE_ODBC_HOME=/home/sekhri/apps/easysoft/
 export DBSHOME=$PWD
-export APPSERVER_PORT=27983
-export ORACLE_HOME=/data/Depend/oracle-10.2.0.1/
-export LD_LIBRARY_PATH=$ODBCHOME/lib:$LOG4CXX/lib:$ORACLE_HOME/lib:$ORACLE_ODBC_HOME/lib:$GSOAPHOME/lib:$LD_LIBRARY_PATH
+export ORACLE_HOME=/home/sekhri/apps/oracle/oracle-10.2.0.1
+export SWIG_HOME=/home/sekhri/apps/swig/
+
+export GSOAPHOME=/home/sekhri/apps/gsoap/
+export LD_LIBRARY_PATH=$ODBCHOME/lib:$LOG4CXX/lib:$ORACLE_HOME/lib:$ORACLE_ODBC_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$SWIG_HOME/bin:$GSOAPHOME/bin:$PATH
 export PYTHONINCLUDE=/usr/include/python2.3/
 export DBSCONFIG=$DBSHOME/etc/server.conf
-export PATH=$SWIG_HOME/bin:$GSOAPHOME/bin:$WSDL2PYHOME/bin:$DBSHOME/bin:$PATH
-#echo "Check Proper swig version (1.3.27 or later) added to path and proper python include to PYTHONINCLUDE"
-#echo "ADD To PYTHONPATH DMS/DBS/prototypes/proto_0/python"
-export PYTHONPATH=$DBSHOME/interface/Python:$DBSHOME/../../Clients/PythonAPI:$DBSHOME/../../Clients/PythonAPI/lib:$WSDL2PYHOME:$PYTHONPATH
+export PATH=$SWIG_HOME/bin:$PATH
+export PYTHONPATH=$DBSHOME/interface/Python:$DBSHOME/../../Clients/PythonAPI:$PYTHONPATH
+
 
