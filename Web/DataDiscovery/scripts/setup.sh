66752ab5086da0dbaf15bc17080d1d4d80c70e17
valya <>
2006-10-07 03:16:07 +0000

    Some tune ups

--- Web/DataDiscovery/scripts/setup.sh
+++ Web/DataDiscovery/scripts/setup.sh
@@ -2,22 +2,23 @@
 
 if [ -z $DLSHOME ]; then
    echo "Please define DLSHOME environment, e.g. /path/COMP/DLS"
-   exit 1
+   exit
 fi
 
 if [ -z $DBSHOME ]; then
    echo "Please define DBSHOME environment, e.g. /path/COMP/DBS"
-   exit 1
+   exit
 fi
 
 if [ -z $DDHOME ]; then
    echo "Please define DBSHOME environment, e.g. /path/COMP/DBS"
-   exit 1
+   exit
 fi
 
 export CVSROOT=:pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories/CMSSW
 export CVSROOT=:kserver:cmscvs.cern.ch:/cvs_server/repositories/CMSSW
-export PYTHONPATH=/data/DBSDataDiscovery/install/lib/python2.4/site-packages:$PYTHONPATH
+ver=`python -V 2>&1 | awk '{split($2,a,"."); print ""a[1]"."a[2]""}'`
+export PYTHONPATH=/data/DBSDataDiscovery/install/lib/python${ver}/site-packages:$PYTHONPATH
 export PYTHONPATH=$DLSHOME/Client/lib:$PYTHONPATH
 export PYTHONPATH=$DBSHOME/Clients/PythonAPI/lib:$DBSHOME/Clients/PythonAPI/:$PYTHONPATH
 export ORACLE_HOME=/afs/cern.ch/project/oracle/linux/10102gcc323/
