d91eaee7ff7ce9c33186809e00d393c0302a86cf
valya <>
2006-10-10 16:28:34 +0000

    Add Dev/fanfani dbs instances

--- Web/DataDiscovery/DBSInst.py
+++ Web/DataDiscovery/DBSInst.py
@@ -63,7 +63,8 @@ DBS_DLS_INST= {
    "MCLocal_3/Writer":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/MCLocal_3"),
    "MCLocal_4/Writer":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/MCLocal_4"),
    "Dev/Writer":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/LFC"),
-   "DevMC/Writer":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/MCLocal_Test")
+   "DevMC/Writer":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/MCLocal_Test"),
+   "Dev/fanfani":("DLS_TYPE_DLI","prod-lfc-cms-central.cern.ch/grid/cms/DLS/MCLocal_Test")
 }
 ################################################################################################
 SITE_LIST=[
