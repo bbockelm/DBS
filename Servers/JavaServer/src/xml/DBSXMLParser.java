383180b385118b80f88efd6797b4dc14e3f78ba2
afaq <>
2006-10-12 17:40:21 +0000

    simple xml parsers for the gen-x server

---
+++ Servers/JavaServer/src/xml/DBSXMLParser.java
@@ -0,0 +1,172 @@
+package dbsxml;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.SAXParser;
+import java.io.*;
+import java.util.Vector;
+import java.util.Hashtable;
+
+public class DBSXMLParser extends DefaultHandler {
+
+  public Vector elements;
+
+  public DBSXMLParser() {
+	        super();
+        elements = new Vector();
+  }
+
+  public Vector getElements(){
+	return(elements);
+	}
+
+  public void startDocument () throws SAXException
+
+  {
+      // Write your application specific logic
+  }
+
+  public void endDocument() throws SAXException {
+
+    // Write your application specific logic
+  }
+
+  public void startElement (String uri, String localName,
+                              String qName, Attributes attributes)
+        throws SAXException{
+        // Write your application specific logic
+        //System.out.println("StartElement"); 
+        //System.out.println("URI :"+uri);
+        //System.out.println("localName:"+localName);
+        //System.out.println("qName: >>>>>>>>>>>>>>>>>>>>>"+qName);
+        Element newElement = new Element(qName); 
+        //newElement.value = "Test";
+        for (int i=0; i< attributes.getLength();++i){
+             //System.out.println("attribute:"+attributes.getQName(i)+" value: "+attributes.getValue(i));
+             newElement.value = "dbsTest";
+             newElement.attributes.put(attributes.getQName(i), attributes.getValue(i)); 
+        } 
+        elements.add(newElement);
+    }
+
+    public void endElement (String uri, String localName, String qName)
+        throws SAXException
+    {
+        //System.out.println("qName:<<<<<<<<<<<<<<<<<<<"+qName);
+    }
+
+    public void characters (char ch[], int start, int length)
+        throws SAXException
+    {
+        //System.out.println("characters");
+        // your application specific logic
+
+		String value="";
+		//System.out.print("Characters: ");
+		for (int i = start; i < start + length; i++) {
+			//System.out.print(ch[i]);
+			switch (ch[i]) {
+				case '\\':
+					//System.out.print("\\\\");
+					break;
+				case '"':
+					//System.out.print("\\\"");
+					break;
+				case '\n':
+					//System.out.print("\\n");
+					break;
+				case '\r':
+					//System.out.print("\\r");
+					break;
+				case '\t':
+					//System.out.print("\\t");
+					break;
+				default:
+				//System.out.print(ch[i]);
+					value=value+ch[i];	
+					break;
+			}
+		}
+        //System.out.println("Value:"+value);       
+    }
+
+  //Parse a String
+  public void parseString(String xmlcontent){
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    try{
+      SAXParser saxParser = factory.newSAXParser();
+      StringReader reader = new StringReader(xmlcontent);
+      InputSource source = new InputSource(reader);
+      saxParser.parse(source, this);
+      //saxParser.parse(newFile,new DefaultHandlerImpl());
+    }catch(javax.xml.parsers.ParserConfigurationException e){
+      e.printStackTrace();
+    }catch(java.io.IOException e){
+      e.printStackTrace();
+    }catch(org.xml.sax.SAXException e){
+      e.printStackTrace();
+    }
+  }
+
+  //Parse a file
+  public void parseFile(String fileName){
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    try{
+      SAXParser saxParser = factory.newSAXParser();
+      File newFile = new File(fileName);
+      saxParser.parse(newFile, this);
+      //saxParser.parse(newFile,new DefaultHandlerImpl());
+    }catch(javax.xml.parsers.ParserConfigurationException e){
+      e.printStackTrace();
+    }catch(java.io.IOException e){
+      e.printStackTrace();
+    }catch(org.xml.sax.SAXException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String args[]){
+    String fileName = "";
+    if(args.length != 1){
+      System.out.println("Usage is: java DBSXMLParser [xml file Name]");
+      System.exit(0);
+    }else{
+      fileName = args[0];
+    }
+    DBSXMLParser dbsParser = new DBSXMLParser();
+    //Parse a file
+    //dbsParser.pareFile(fileName);
+
+    //Parse a String
+    String xml_string = "<?xml version='1.0' standalone='yes'?>" +
+"<dbs>"+
+"<primary-dataset id='1' annotation='First Primary in new era' primary_name='PrimaryDS_ANZAR_01' trigger_path_description='null' mc_channel_description='null' mc_production='null' mc_decay_chain='null' other_description='null' start_date='2006-10-05' end_date='2007-10-05' file_type='null' created_by='null' creation_date='null' last_modification_by='null' last_modified_by='null'/>"+
+"<processed-dataset id='1' path='/PrimaryDS_ANZAR_01/SIM/anzar-procds-01' open_for_writing='y' creation_date='null' last_modification_date='null' run_number='null' first_event_number='null' last_event_number='null' start_of_run='null' end_of_run='null' physics_group_name='null' physics_group_convener='null'/>"+
+"<processed-dataset id='1' path='/PrimaryDS_ANZAR_01/RECO/anzar-procds-02' open_for_writing='y' creation_date='null' last_modification_date='null' run_number='null' first_event_number='null' last_event_number='null' start_of_run='null' end_of_run='null' physics_group_name='null' physics_group_convener='null'/>"+
+"<test-parent-element attrb='1'>"+
+"<test-child-element attrb='11'/>"+
+"<test-child-element attrb='12'/>"+
+"</test-parent-element>"+
+"</dbs>";
+
+    dbsParser.parseString(xml_string); 
+    
+    Vector allElement = dbsParser.getElements();
+    for (int i=0; i<allElement.size(); ++i) {
+        Element e = (Element)allElement.elementAt(i);
+        String name = e.name;
+        if (name == "primary-dataset" ) {
+           System.out.println("Found a primary dataset: "+name);  
+           Hashtable atribs = e.attributes;
+           String primary_name = (String)atribs.get("primary_name");
+           System.out.println("Name of primarydataset: "+primary_name);
+        } 
+    }
+
+  }
+
+}
+
---
+++ Servers/JavaServer/src/xml/Element.java
@@ -0,0 +1,25 @@
+package dbsxml;
+
+import java.util.Hashtable;
+
+public class Element{
+
+	public String name;
+	public String value;
+        public Hashtable attributes;
+        
+
+	public Element(String name,String value){
+	
+		this.name=name;
+		this.value=value;
+                attributes = new Hashtable(); 
+	}
+
+       public Element(String name) {
+                this.name=name;
+                attributes = new Hashtable(); 
+       } 
+
+}
+
