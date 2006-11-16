2fee850d40824e98769892ace31cd38587f8a508
afaq <>
2006-11-16 23:05:43 +0000

    deployable ddl for mysql

--- Schema/NeXtGen/gen_auto_inc.sh
+++ Schema/NeXtGen/gen_auto_inc.sh
@@ -1,12 +1,11 @@
 # DBS-NeXtGen-MySQL.sql is the .sql file generated from 
 # ../DBS-NeXtGen-MySQL.druid using DRUID Tool
 
-mv DBS-NeXtGen-MySQL.sql DBS-NeXtGen-MySQL.sql.BAK
 echo 
 echo "   Inserting auto_increment for mysql"
-cat DBS-NeXtGen-MySQL.sql.BAK | sed -e "s%ID                    int%ID                    int not null auto_increment%g" > DBS-NeXtGen-MySQL.sql
-echo "   Deploy DBS-NeXtGen-MySQL.sql to MySQL"
-echo "   mysql -u username -ppassword  ...  < DBS-NeXtGen-MySQL.sql"
+cat DBS-NeXtGen-MySQL.sql | sed -e "s%ID                    int%ID                    int not null auto_increment%g" > DBS-NeXtGen-MySQL_DEPLOYABLE.sql
+echo "   Deploy DBS-NeXtGen-MySQL_DEPLOYABLE.sql to MySQL"
+echo "   mysql -u username -ppassword  ...  < DBS-NeXtGen-MySQL_DEPLOYABLE.sql"
 echo
 
  
