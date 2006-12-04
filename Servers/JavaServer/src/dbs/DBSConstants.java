810a92b1b8f763741900aa6eba42200412d2d44d
afaq <>
2006-12-04 15:24:30 +0000

    DBSConstants will noty contains any passwords etc, passwords and other config params go in dbs.config

--- Servers/JavaServer/src/dbs/DBSConstants.java
+++ Servers/JavaServer/src/dbs/DBSConstants.java
@@ -1,7 +1,9 @@
 /**
- * @author sekhri
+ * 
+ $Revision: 1.24 $"
+ $Id: DBSServlet.java,v 1.24 2006/11/30 16:29:36 sekhri Exp $"
  *
- */
+*/
 package dbs;
 public class DBSConstants {
 	/**
@@ -15,34 +17,5 @@ public class DBSConstants {
 	public static String XML_WARNNING_HEADER = "<warnning>\n";
 	public static String XML_WARNNING_FOOTER = "</warnning>\n";
 	public static String XML_SUCCESS = "<SUCCESS/>\n";
-
-        
-	public static String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static String URL = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(PORT=10520)(HOST=oradev10.cern.ch))(CONNECT_DATA=(SID=D10)))";
-	public static String USERID = "cms_dbs_afaq";
-	public static String PASSWORD = "MeraMaslaHaina?";
-       
-
-
-	
-	/*
-	public static String DRIVER = "org.gjt.mm.mysql.Driver";
-	public static String URL = "jdbc:mysql://localhost/dbs_new_era_v04";
-	public static String USERID = "anzar";
-	public static String PASSWORD = "prodagentpass";
-        /*
-
-	public static String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static String URL = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(PORT=1521)(HOST=uscmsdb03.fnal.gov))(CONNECT_DATA=(SID=cmscald)))";
-	public static String USERID = "cms_reader";
-	public static String PASSWORD = "reader";
-
-
-	
-	public static String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static String URL = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP) (HOST=cmsr1-v.cern.ch) (PORT=10121))(ADDRESS=(PROTOCOL=TCP)(HOST=cmsr2-v.cern.ch)(PORT=10121))(ADDRESS=(PROTOCOL=TCP)(HOST=cmsr3-v.cern.ch) (PORT=10121))(ADDRESS=(PROTOCOL=TCP)(HOST=cmsr4-v.cern.ch)(PORT=10121))(LOAD_BALANCE=yes)(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=cms_dbs_mcprod_global)(FAILOVER_MODE=(TYPE=SELECT)(METHOD=BASIC)(RETRIES=200)(DELAY=15))))";
-	public static String USERID = "cms_dbs_mcprod_local_writer";
-	public static String PASSWORD = "ic_2onograph";
-	*/
 
 }
