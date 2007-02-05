e977de58c0f9fcd3696c674eae3f158e66ff2b38
afaq <>
2007-02-05 19:33:28 +0000

    update run call added to python api

--- Clients/Python/DBSAPI/dbsApi.py
+++ Clients/Python/DBSAPI/dbsApi.py
@@ -1521,7 +1521,58 @@ class DbsApi(DbsConfig):
     data = self._server._call ({ 'api' : 'insertRun',
                          'xmlinput' : xmlinput }, 'POST')
     logging.debug(data)
- 
+
+
+# ------------------------------------------------------------
+
+  def updateRun(self, run):
+
+    funcInfo = inspect.getframeinfo(inspect.currentframe())
+    logging.debug("Api call invoked %s" % str(funcInfo[2]))
+
+    """
+    Updates a run in the DBS databse.
+
+    param:
+        run : The dbs run passed in as DbsRun object. RunNumber is mandatory
+	      Following values could be updated, one can provide one or all
+		NumberOfEvents, NumberOfLumiSections, TotalLuminosity, EndOfRun
+
+    raise: DbsApiException, DbsBadRequest, DbsBadData, DbsNoObject, DbsExecutionError, DbsConnectionError,
+           DbsToolError, DbsDatabaseError, DbsBadXMLData, InvalidDatasetPathName, DbsException
+
+    examples:
+
+         run = DbsRun (
+                 RunNumber=1,
+                 NumberOfEvents= 100,
+                 NumberOfLumiSections= 20,
+                 TotalLuminosity= 2222,
+                 EndOfRun= 'never',
+         )
+
+         api.insertRun (run)
+
+    """
+    xmlinput  = "<?xml version='1.0' standalone='yes'?>"
+    xmlinput += "<dbs>"
+    xmlinput += "<run"
+    xmlinput += " run_number='"+str(run.get('RunNumber', ''))+"'"
+    xmlinput += " number_of_events='"+str(run.get('NumberOfEvents', ''))+"'"
+    xmlinput += " number_of_lumi_sections='"+str(run.get('NumberOfLumiSections', ''))+"'"
+    xmlinput += " total_luminosity='"+str(run.get('TotalLuminosity', ''))+"'"
+    xmlinput += " end_of_run='"+run.get('EndOfRun', '')+"'"
+    xmlinput += " />"
+    xmlinput += "</dbs>"
+
+    logging.debug(xmlinput)
+    if self.verbose():
+       print "insertRun, xmlinput",xmlinput
+
+    data = self._server._call ({ 'api' : 'updateRun',
+                         'xmlinput' : xmlinput }, 'POST')
+    logging.debug(data)
+
   # ------------------------------------------------------------
   def insertFiles(self, dataset, files, block):
     """ 
