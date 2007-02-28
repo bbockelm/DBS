28790536dae849f0d96dcc572681b8b76d619bee
valya <>
2007-02-28 15:37:42 +0000

    Example to uyse visibility switch

--- Web/DataDiscovery/js/setcontent.js
+++ Web/DataDiscovery/js/setcontent.js
@@ -1,2 +1,3 @@
 document.write('<style type="text/css"> div.normalcontent { display:inline } </style>' )
+//document.write('<style type="text/css"> div.normalcontent { visibility:visible } </style>' )
 
