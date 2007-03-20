d5d4335949bab8a97e89a0175d2d05a7dd99f366
afaq <>
2007-03-20 21:15:41 +0000

    Added alisa for dbsCommandLine, makes life very easy to look at DBS data

--- Clients/Python/setup.sh
+++ Clients/Python/setup.sh
@@ -3,3 +3,16 @@ export CLIENTHOME=$PWD
 export PYTHONPATH=$CLIENTHOME:$PYTHONPATH
 export DBS_CLIENT_CONFIG=$PWD/DBSAPI/dbs.config
 
+# Setup the ALIAS for dbsCommandLine.py
+export DBSCMD_HOME=$CLIENTHOME/DBSAPI
+#Primary Datasets
+alias dbslsp='$DBSCMD_HOME/dbsCommandLine.py -c lsp'
+#Processed Datasets
+alias dbslsd='$DBSCMD_HOME/dbsCommandLine.py -c lsd'
+#Algorithms
+alias dbslsa='$DBSCMD_HOME/dbsCommandLine.py -c lsa'
+#Files
+alias dbslsf='$DBSCMD_HOME/dbsCommandLine.py -c lsf'
+#Blocks
+alias dbslsb='$DBSCMD_HOME/dbsCommandLine.py -c lsb'
+
