7d05c23476ec65f5d748d7aeb6300c8531270076
afaq <>
2007-03-23 14:56:30 +0000

    permission change in setup.sh

--- Clients/Python/setup.sh
+++ Clients/Python/setup.sh
@@ -5,14 +5,25 @@ export DBS_CLIENT_CONFIG=$PWD/DBSAPI/dbs.config
 
 # Setup the ALIAS for dbsCommandLine.py
 export DBSCMD_HOME=$CLIENTHOME/DBSAPI
+#
+# DBS Commandline tool
+#
+#
+chmod +x $DBSCMD_HOME/dbsCommandLine.py 
+#
+alias dbs='$DBSCMD_HOME/dbsCommandLine.py -c '
+#
+#
 #Primary Datasets
-alias dbslsp='$DBSCMD_HOME/dbsCommandLine.py -c lsp'
+#alias dbslsp='$DBSCMD_HOME/dbsCommandLine.py -c lsp'
 #Processed Datasets
-alias dbslsd='$DBSCMD_HOME/dbsCommandLine.py -c lsd'
+#alias dbslsd='$DBSCMD_HOME/dbsCommandLine.py -c lsd'
 #Algorithms
-alias dbslsa='$DBSCMD_HOME/dbsCommandLine.py -c lsa'
+#alias dbslsa='$DBSCMD_HOME/dbsCommandLine.py -c lsa'
 #Files
-alias dbslsf='$DBSCMD_HOME/dbsCommandLine.py -c lsf'
+#alias dbslsf='$DBSCMD_HOME/dbsCommandLine.py -c lsf'
 #Blocks
-alias dbslsb='$DBSCMD_HOME/dbsCommandLine.py -c lsb'
+#alias dbslsb='$DBSCMD_HOME/dbsCommandLine.py -c lsb'
+#SE
+#alias dbslsse='$DBSCMD_HOME/dbsCommandLine.py -c lsse'
 
