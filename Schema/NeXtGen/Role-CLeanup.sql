6b77a7ed0d77fc98e03d0b460763bc7a90284c5f
yuyi <>
2007-04-02 19:36:08 +0000

    Drop all the roles. YG

---
+++ Schema/NeXtGen/Role-CLeanup.sql
@@ -0,0 +1,22 @@
+-- =================
+--
+--  drop the ROLES
+--
+-- =================
+PROMPT Dropping Role 'CMS_DBS_SchemaNameAbbr_ADMIN_ROLE'
+drop role  CMS_DBS_SchemaNameAbbr_ADMIN_ROLE
+/
+
+PROMPT Dropping Role 'CMS_DBS_SchemaNameAbbr_WRITER_ROLE'
+drop role CMS_DBS_SchemaNameAbbr_WRITER_ROLE
+/
+
+PROMPT Dropping Role 'CMS_DBS_SchemaNameAbbr_READER_ROLE'
+drop role CMS_DBS_SchemaNameAbbr_READER_ROLE
+/
+
+PROMPT Dropping Role 'CMS_DBS_SchemaNameAbbr_DLS_ROLE'
+drop role  CMS_DBS_SchemaNameAbbr_DLS_ROLE 
+/
+
+
