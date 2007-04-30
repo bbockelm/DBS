2e291c530e0b45b4730bca1e443a8adea294129a
valya <>
2007-04-30 14:36:20 +0000

    Added site search into physicists view, after discussion on workshop

--- Web/DataDiscovery/js/userFooter.js
+++ Web/DataDiscovery/js/userFooter.js
@@ -4,6 +4,7 @@ function footerMenuText(){
 {label: "Finder", link: "_finder?userMode=user", title: "Finder: query-driven search"},
 {label: "Analysis", link: "_analysis?userMode=user", title: "Search analysis datasets"},
 //{label: "RSS", link: "_rss?userMode=user", title: "RSS Feeds publish frequently updates about your data"},
+{label: "Site Search", link: "_siteSearch?userMode=user", title: "Site search: site-based search"},
 {label: "Help", link: "_help?userMode=user", title: "Help: DBS glossary, terms, feedback form"},
 {label: "Contact", link: "_contact?userMode=user", title: "Contact DBS support team"},
 {title: "Physicist view of DBS", link: "index?userMode=user", label: "Physicist", position: "right"},
