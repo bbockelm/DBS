4eb510a950c1b3bf62648f3883c4962ffc9e674a
afaq <>
2007-07-17 15:36:23 +0000

    turn off server logging

--- Servers/JavaServer/src/dbs/DBSConstants.java
+++ Servers/JavaServer/src/dbs/DBSConstants.java
@@ -1,7 +1,7 @@
 /**
  * 
- $Revision: 1.36 $"
- $Id: DBSConstants.java,v 1.36 2007/06/20 22:03:47 afaq Exp $"
+ $Revision: 1.37 $"
+ $Id: DBSConstants.java,v 1.37 2007/06/22 19:41:47 afaq Exp $"
  *
 */
 package dbs;
@@ -12,8 +12,8 @@ package dbs;
 */
 public class DBSConstants {
         //Specify the Global debug flag, if set to True DEBUG Messages wil be displayed. 
-	//public static boolean DEBUG = false;
-	public static boolean DEBUG = true;
+	public static boolean DEBUG = false;
+	//public static boolean DEBUG = true;
 	public static String XML_HEADER =  "<?xml version='1.0' standalone='yes'?>\n<!-- DBS Version 1 -->\n<dbs>\n";
 	public static String XML_FOOTER = "</dbs>\n";
 	public static String XML_EXCEPTION_HEADER = "<exception>\n";
