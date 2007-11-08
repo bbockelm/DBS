454f2e898b03ff854a82bf330664bd128b0ff4bd
sekhri <>
2007-11-08 00:56:57 +0000

    fix for anzar

---
+++ Servers/JavaServer/src/dm/DbsDataset.java
@@ -0,0 +1,12 @@
+package dm;
+
+public class DbsDataset {
+	private String datasetName;
+	public String getDatasetName() {
+		return datasetName;
+	}	
+	public void setDatasetName(String datasetName) {
+		this.datasetName = datasetName;
+	}
+		
+}
---
+++ Servers/JavaServer/src/dm/DbsInstance.java
@@ -0,0 +1,12 @@
+package dm;
+
+public class DbsInstance {
+	private String instanceName;
+	public String getInstanceName() {
+		return instanceName;
+	}	
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+	}
+		
+}
