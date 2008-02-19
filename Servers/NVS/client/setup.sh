2d3d65493ed9beb4b1715cde8e78a1ad24fe5dc4
sekhri <>
2008-02-19 20:59:56 +0000

    fixed setup.sh file

--- Servers/NVS/client/setup.sh
+++ Servers/NVS/client/setup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 export CLIENTHOME=$PWD
 export PYTHONPATH=$CLIENTHOME/service:$CLIENTHOME/servlet:$PYTHONPATH
-export NVS_CLIENT_CONFIG=$PWD/service/nvs.config
+export NVS_CLIENT_CONFIG=$PWD/servlet/nvs.config
 
 #
 #
