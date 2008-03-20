f94a0de1deed3b956cf924dc6bab9d16b97b66af
yuyi <>
2008-03-20 17:01:46 +0000

    Add create views. YG

---
+++ Schema/NeXtGen/view_templates.sql
@@ -0,0 +1,169 @@
+PROMPT Create View  FileSummary
+
+Drop view FileSummary
+/
+
+CREATE VIEW FileSummary (LogicalFileName, CreationDate, CreatedBy, Checksum, NumberOfEvents, 
+FileSize, Type, Status) AS SELECT tf.LogicalFileName, tf.CreationDate, tp.DistinguishedName, 
+tf.Checksum, tf.NumberOfEvents, tf.FileSize, tft.Type, tfs.Status 
+FROM Files tf 
+JOIN FileType tft ON tf.FileType=tft.ID 
+JOIN FileStatus tfs ON tf.FileStatus=tfs.ID JOIN person tp ON tf.CreatedBy = tp.ID
+/
+
+PROMPT Create VIEW ReleaseSummary
+
+drop VIEW ReleaseSummary
+/
+
+CREATE VIEW ReleaseSummary (Version, CreationDate, CreatedBy, FamilyName, ExecutableName) 
+AS SELECT tav.Version, tav.CreationDate, tp.DistinguishedName, taf.FamilyName, tae.ExecutableName 
+FROM AlgorithmConfig tac JOIN AppFamily taf ON taf.ID=tac.ApplicationFamily 
+JOIN AppVersion tav ON tav.ID=tac.ApplicationVersion 
+JOIN AppExecutable tae ON tae.ID=tac.ExecutableName 
+JOIN person tp ON tav.CreatedBy = tp.ID
+/
+
+PROMPT Create VIEW RunSummary
+
+drop VIEW RunSummary
+/
+
+CREATE VIEW RunSummary (RunNumber, CreationDate, CreatedBy, TotLumi, StoreNumber, StartOfRun, EndOfRun, StartEvent, EndEvent, NumberOfLumis) 
+AS SELECT tr.RunNumber, tr.CreationDate, tp.DistinguishedName, tr.TotalLuminosity, tr.StoreNumber, tr.StartOfRun, tr.EndOfRun, 
+tls.StartEventNumber, tls.EndEventNumber, count(tls.LumiSectionNumber) 
+FROM Runs tr JOIN LumiSection tls ON tr.ID=tls.RunNumber 
+JOIN person tp ON tr.CreatedBy = tp.ID 
+GROUP BY tr.RunNumber, tr.CreationDate, tp.DistinguishedName, tr.TotalLuminosity, 
+tr.StoreNumber, tr.StartOfRun, tr.EndOfRun, tls.StartEventNumber, tls.EndEventNumber
+/
+
+PROMPT Create VIEW RunManagerSummary
+
+drop VIEW RunManagerSummary
+/
+
+CREATE VIEW RunManagerSummary (RunNumber, CreationDate, CreatedBy, TotLumi, StoreNumber, StartOfRun, EndOfRun, StartEvent, 
+EndEvent, Path, NumberOfLumis, NumberOfFiles) 
+AS SELECT DISTINCT tr.RunNumber, tr.CreationDate, tp.DistinguishedName, tr.TotalLuminosity, 
+tr.StoreNumber, tr.StartOfRun, tr.EndOfRun, tls.StartEventNumber, tls.EndEventNumber, tblk.Path, 
+count(DISTINCT tls.LumiSectionNumber), count(tf.LogicalFileName) 
+FROM Runs tr JOIN LumiSection tls ON tr.ID=tls.RunNumber 
+JOIN FileRunLumi tfrl ON tfrl.Run=tr.ID 
+JOIN Files tf ON tf.ID=tfrl.FileId 
+JOIN Block tblk ON tblk.ID=tf.Block 
+JOIN person tp ON tr.CreatedBy = tp.ID 
+GROUP BY tr.RunNumber, tr.CreationDate, tp.DistinguishedName, tr.TotalLuminosity, 
+tr.StoreNumber, tr.StartOfRun, tr.EndOfRun, tls.StartEventNumber, tls.EndEventNumber, tblk.Path
+/
+
+PROMPT Create VIEW SiteSummary
+
+drop VIEW SiteSummary
+/
+
+CREATE VIEW SiteSummary (SEName, CreationDate, CreatedBy, NumberOfProcDS) 
+AS SELECT tse.SEName, tse.CreationDate, tp.DistinguishedName, count(DISTINCT tblk.Path) 
+FROM StorageElement tse 
+JOIN SEBlock tseb ON tseb.SEID=tse.ID 
+JOIN Block tblk ON tblk.ID=tseb.BlockID 
+JOIN person tp ON tse.CreatedBy = tp.ID 
+GROUP BY tse.SEName, tse.CreationDate, tp.DistinguishedName
+/
+
+PROMPT CREATE VIEW PrimSummary
+
+drop VIEW PrimSummary
+/
+
+CREATE VIEW PrimSummary (Name, CreationDate, CreatedBy, PrimType, NumberOfProcDS) 
+AS SELECT tprm.Name, tprm.CreationDate, tp.DistinguishedName, tprmt.Type, count(tprd.Name) 
+FROM PrimaryDataset tprm 
+JOIN PrimaryDSType tprmt ON tprmt.ID=tprm.Type 
+JOIN ProcessedDataset tprd ON tprm.ID=tprd.PrimaryDataset 
+JOIN person tp ON tprm.CreatedBy = tp.ID 
+GROUP BY tprm.Name, tprm.CreationDate, tp.DistinguishedName, tprmt.Type
+/
+
+PROMPT CREATE VIEW ProcSummary
+
+drop VIEW ProcSummary
+/
+
+CREATE VIEW ProcSummary (Name, CreationDate, CreatedBy, NumberOfBlocks, BlocksSize, NumberOfFiles, NumberOfEvents) 
+AS SELECT tprd.Name, tprd.CreationDate, tp.DistinguishedName, count(tblk.Name), 
+sum(tblk.BlockSize), sum(tblk.NumberOfFiles), sum(tblk.NumberOfEvents) 
+FROM ProcessedDataset tprd 
+JOIN Block tblk ON tprd.ID=tblk.Dataset 
+JOIN person tp ON tprd.CreatedBy = tp.ID 
+GROUP BY tprd.Name, tprd.CreationDate, tp.DistinguishedName
+/
+
+PROMPT CREATE VIEW TierSummary
+
+
+drop VIEW TierSummary
+/
+
+CREATE VIEW TierSummary (Name, CreationDate, CreatedBy, NumberOfProcDS) 
+AS SELECT tdt.Name, tdt.CreationDate, tp.DistinguishedName, count(tprd.Name) 
+FROM ProcessedDataset tprd 
+JOIN ProcDSTier tpdst ON tprd.ID=tpdst.Dataset 
+JOIN DataTier tdt ON tpdst.DataTier=tdt.ID 
+JOIN person tp ON tdt.CreatedBy = tp.ID GROUP BY tdt.Name, tdt.CreationDate, tp.DistinguishedName
+
+/
+
+PROMPT CREATE VIEW DatasetSummary
+
+drop VIEW DatasetSummary
+/
+
+CREATE VIEW DatasetSummary (Path, CreationDate, CreatedBy, TotalSize, NumberOfBlocks, NumberOfFiles, 
+NumberOfEvents, NumberOfSites) 
+AS SELECT DISTINCT tblk.Path, tprd.CreationDate, tp.DistinguishedName, sum(tblk.BlockSize), count(tblk.Name), 
+sum(tblk.NumberOfFiles), sum(tblk.NumberOfEvents), count(tse.SEName) 
+FROM processeddataset tprd 
+JOIN block tblk ON tblk.Dataset = tprd.ID 
+JOIN primarydataset tpm ON tprd.PrimaryDataset = tpm.ID 
+LEFT OUTER JOIN seblock tseb ON tseb.BlockID = tblk.ID 
+LEFT OUTER JOIN storageelement tse ON tseb.SEID = tse.ID 
+JOIN person tp ON tprd.CreatedBy = tp.ID GROUP BY tblk.Path, tprd.CreationDate, tp.DistinguishedName
+/
+
+PROMPT Grant select on FileSummary to  '@build.schema.owner.name@_READER'
+Grant select on FileSummary to @build.schema.owner.name@_READER
+/
+
+
+PROMPT Grant select on ReleaseSummary to  '@build.schema.owner.name@_READER'
+Grant select on ReleaseSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on RunSummary to  '@build.schema.owner.name@_READER'
+Grant select on RunSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on RunManagerSummary to  '@build.schema.owner.name@_READER'
+Grant select on RunManagerSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on SiteSummary to  '@build.schema.owner.name@_READER'
+Grant select on SiteSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on PrimSummary to  '@build.schema.owner.name@_READER'
+Grant select on PrimSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on ProcSummary to  '@build.schema.owner.name@_READER'
+Grant select on ProcSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on TierSummary to  '@build.schema.owner.name@_READER'
+Grant select on TierSummary to @build.schema.owner.name@_READER
+/
+
+PROMPT Grant select on DatasetSummary to  '@build.schema.owner.name@_READER'
+Grant select on DatasetSummary to @build.schema.owner.name@_READER
+/
