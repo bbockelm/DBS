75def84502b107abd0fc1d5137f9a53e939a1780
sekhri <>
2008-06-20 20:44:40 +0000

    Count query for Valentin

--- Servers/JavaServer/src/dbs/search/qb/QueryBuilder.java
+++ Servers/JavaServer/src/dbs/search/qb/QueryBuilder.java
@@ -428,7 +428,7 @@ public class QueryBuilder {
 			}
 			if(db.equals("oracle")) {
 				bindIntValues.add(new Integer(eInt));
-				query =  "SELECT * from (SELECT x.*, rownum as rnum FROM (\n" + query + "\n) x) where rnum between ? and ?";
+				query =  "SELECT * FROM (SELECT x.*, rownum as rnum FROM (\n" + query + "\n) x) where rnum between ? and ?";
 			}
 		}
 
