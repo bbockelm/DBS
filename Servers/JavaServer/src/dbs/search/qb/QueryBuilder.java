b794c622dcc62d0cd517af79e1003bd2a36b05a8
sekhri <>
2008-07-16 15:13:25 +0000

    pagination reverted back to original format

--- Servers/JavaServer/src/dbs/search/qb/QueryBuilder.java
+++ Servers/JavaServer/src/dbs/search/qb/QueryBuilder.java
@@ -510,7 +510,7 @@ public class QueryBuilder {
 			}
 			if(db.equals("oracle")) {
 				bindIntValues.add(new Integer(eInt));
-				query =  "SELECT * FROM (SELECT x.* FROM (\n" + query + "\n) x) where rownum between ? and ?";
+				query =  "SELECT * FROM (SELECT x.*, rownum as rnum FROM (\n" + query + "\n) x) where rnum between ? and ?";
 			}
 		}
 
