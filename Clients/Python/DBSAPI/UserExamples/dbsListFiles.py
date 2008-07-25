afe714f5762ad4ca3bb279bdf2cd1db208ff5a3a
sekhri <>
2008-07-25 16:30:19 +0000

    Not sure why Lee is getting XML from command line

--- Clients/Python/DBSAPI/UserExamples/dbsListFiles.py
+++ Clients/Python/DBSAPI/UserExamples/dbsListFiles.py
@@ -27,7 +27,7 @@ try:
   #for afile in api.listFiles(analysisDataset="/RelVal131QCD_pt15_20/CMSSW_1_3_1-1176201507/GEN-SIM-DIGI-RECO/ALLFILES"):
   #for afile in api.listFiles("", "test_primary_001", "TestProcessedDS001", ['GEN', 'SIM'], "", "", "", False):
   #for afile in api.listFiles(path="/test_primary_001/TestProcessedDS001/GEN-SIM"):
-  allowedRetriveValue = ['all',
+  allowedRetriveValue = [#'all',
 		    #'retrive_invalid_files', 
 		    #'retrive_status',
 		    #'retrive_type',
@@ -38,14 +38,15 @@ try:
 		    #'retrive_child',
 		    #'retrive_algo',
 		    #'retrive_tier',
-		    #'retrive_lumi',
+		    'retrive_lumi',
 		    #'retrive_lumi_excluded',
-		    #'retrive_run',
+		    'retrive_run',
 		    #'retrive_branch',
 		    ]
 
   #for afile in api.listFiles(path="/Upsilon2S/Summer08_STARTUP_V2_Upsilon2S_v3/GEN-SIM-RAW", retriveList=allowedRetriveValue, otherDetails = False):
-  for afile in api.listFiles(path="/RelValMinBias/CMSSW_2_1_0_pre8-RelVal-STARTUP_V4_InitialLumiPileUp_v1-2nd-STARTUP_V4/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO", retriveList=allowedRetriveValue, otherDetails = True):
+  #for afile in api.listFiles(path="/RelValMinBias/CMSSW_2_1_0_pre8-RelVal-STARTUP_V4_InitialLumiPileUp_v1-2nd-STARTUP_V4/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO", retriveList=allowedRetriveValue, otherDetails = True):
+  for afile in api.listFiles(patternLFN="/store/Qdata/h2tb2007/testbeam_HCalAlone/RAW/default_v1/h2.00033990.9999.edm.storageManager.0.0000.root", retriveList=allowedRetriveValue):
   #for afile in api.listFiles(patternLFN="/store/mc/2007/10/1/HLT-Wmunu-1191261655/0005/E2E68481-EE86-DC11-81A7-0019B9E4FE51.root", details=True):
   #for afile in api.listFiles(analysisDataset="/test00_testbeam_HCalEcalCombined/h2tb2007_default_v1/RAW/VijayTestADS", retriveList=allowedRetriveValue):
      print "  %s" % afile
