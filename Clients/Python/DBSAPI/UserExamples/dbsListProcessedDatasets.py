d6a94191f884c86cb08619d8eda80e8837eac2af
sekhri <>
2008-08-22 21:30:19 +0000

    tagging DBS_2_0_0

--- Clients/Python/DBSAPI/UserExamples/dbsListProcessedDatasets.py
+++ Clients/Python/DBSAPI/UserExamples/dbsListProcessedDatasets.py
@@ -18,8 +18,21 @@ try:
   # List all parameter sets
   print "Processed Datasets:"
   #for proc in api.listProcessedDatasets("test_primary_001", "*", "TestProcessedDS002"):
-  for proc in api.listProcessedDatasets("*"):
-     print "  %s" % proc
+  #for proc in api.listProcessedDatasets(patternPrim="Cosmics",  patternProc = "Commissioning08-CRUZET4_v1"):
+  #/RelValMinBias/CMSSW_2_1_2_STARTUP_V5_v3/GEN-SIM-RECO
+  path = '/RelValMinBias/CMSSW_2_1_2_STARTUP_V5_v3/GEN-SIM-RECO'
+  params = path.split('/')
+  for proc in api.listProcessedDatasets(patternPrim=params[1],  patternProc = params[2]):
+     algoList =  proc['AlgoList']
+     for aAlgo in algoList:
+	     print aAlgo['ApplicationVersion']
+	     print aAlgo['ParameterSetID']['Hash']
+	     print aAlgo['ApplicationFamily']
+	     print aAlgo['ExecutableName']
+	     tmpAlgoList =  api.listAlgorithms( patternVer=aAlgo['ApplicationVersion'], patternFam=aAlgo['ApplicationFamily'], patternExe=aAlgo['ExecutableName'], patternPS=aAlgo['ParameterSetID']['Hash'])
+	     for atmpAlgo in tmpAlgoList :
+		     print atmpAlgo['ParameterSetID']['Content']
+  
   
 except DbsApiException, ex:
   print "Caught API Exception %s: %s "  % (ex.getClassName(), ex.getErrorMessage() )
