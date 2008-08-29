a84ff9e008f5ea3d950e273796609ec520ba8579
valya <>
2008-08-29 18:53:55 +0000

    Added rest service and refactor code

--- Web/DataDiscovery/Controllers/DataDiscovery/__init__.py
+++ Web/DataDiscovery/Controllers/DataDiscovery/__init__.py
@@ -2,7 +2,8 @@ from Framework import Controller
 from Framework.PluginManager import DeclarePlugin
 from cherrypy import expose
 
-from DDServer import *
+from services.dd.DDServer import *
+from services.rest.DDRestServer import *
 
-#DeclarePlugin ("/Controllers/DataDiscovery/DDServer", DDServer, {"baseUrl": "/"})
 DeclarePlugin ("/Controllers/DataDiscovery/DDServer", DDServer, {"baseUrl": "/DDServer"})
+DeclarePlugin ("/Controllers/DataDiscovery/DDRestServer", DDRestServer, {"baseUrl": "/services"})
