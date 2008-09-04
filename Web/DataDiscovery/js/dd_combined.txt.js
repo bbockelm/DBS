43c4d3e5b3fbd33c3e600a7f8d8f351d66311254
valya <>
2008-09-04 20:16:39 +0000

    Pass host name to JS, this will allow to create full URL path, rather then relative links. Fix problem with masthead links for REST services

--- Web/DataDiscovery/js/dd_combined.txt.js
+++ Web/DataDiscovery/js/dd_combined.txt.js
@@ -1,17 +1,21 @@
-/*['js/masthead_footer.js\n', 'js/userName.js\n', 'js/utils.js\n', 'js/yui_utils.js\n', 'js/prototype.js\n', 'js/rico.js\n', 'js/ajax_init.js\n', 'js/dhtml_history/dhtmlHistory.js\n']*/function _footerMenuText(mode){
+/*['js/masthead_footer.js\n', 'js/userName.js\n', 'js/utils.js\n', 'js/yui_utils.js\n', 'js/prototype.js\n', 'js/rico.js\n', 'js/ajax_init.js\n', 'js/dhtml_history/dhtmlHistory.js\n']*/function _footerMenuText(host,mode){
+    // always check provided host and append / at the end
+    if (host[host.length]!='/') {
+        host=host+'/';
+    }
     return [
-{label: "Home", link: "_advanced?userMode="+mode, title: "DBS Data Discovery home page"},
-{label: "aSearch", link: "_advanced?userMode="+mode, title: "Advanced keyword search"},
-{label: "Navigator", link: "_navigator?userMode="+mode, title: "Navigator: menu-driven search"},
+{label: "Home", link: host+"_advanced?userMode="+mode, title: "DBS Data Discovery home page"},
+{label: "aSearch", link: host+"_advanced?userMode="+mode, title: "Advanced keyword search"},
+{label: "Navigator", link: host+"_navigator?userMode="+mode, title: "Navigator: menu-driven search"},
 //{label: "Finder", link: "_finder?userMode="+mode, title: "Finder: query-driven search"},
 //{label: "Analysis", link: "_analysis?userMode="+mode, title: "Search analysis datasets"},
-{label: "RSS", link: "_rss?userMode="+mode, title: "RSS Feeds publish frequently updates about your data"},
-{label: "Sites", link: "_siteSearch?userMode="+mode, title: "Site search: site-based search"},
-{label: "Runs", link: "_runs?userMode="+mode, title: "Run search: run-based search"},
+{label: "RSS", link: host+"_rss?userMode="+mode, title: "RSS Feeds publish frequently updates about your data"},
+{label: "Sites", link: host+"_siteSearch?userMode="+mode, title: "Site search: site-based search"},
+{label: "Runs", link: host+"_runs?userMode="+mode, title: "Run search: run-based search"},
 //{label: "Pages", link: "_pages?userMode="+mode, title: "Data discovery world-wide pages"},
-{label: "Admin", link: "_admin?userMode=expert", title: "Administrate dataset/block/LFNs in DBS"},
-{label: "Help", link: "_help?userMode="+mode, title: "Help: DBS glossary, terms, feedback form"},
-{label: "Contact", link: "_contact?userMode="+mode, title: "Contact DBS support team"},
+{label: "Admin", link: host+"_admin?userMode=expert", title: "Administrate dataset/block/LFNs in DBS"},
+{label: "Help", link: host+"_help?userMode="+mode, title: "Help: DBS glossary, terms, feedback form"},
+{label: "Contact", link: host+"_contact?userMode="+mode, title: "Contact DBS support team"},
 {label: "View", id:"", link: "javascript:ChangeView()", title: "Define the data view", position: "right"}
            ]
 }
