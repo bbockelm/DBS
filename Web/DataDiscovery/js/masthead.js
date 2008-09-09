e9ca12f9cb699834af50319239557e49c85e13b4
valya <>
2008-09-09 13:33:43 +0000

    Put back Phedex link since DataTransfer is not ready yet

--- Web/DataDiscovery/js/masthead.js
+++ Web/DataDiscovery/js/masthead.js
@@ -139,8 +139,8 @@ function menuText(){
 	{title: "CMS Dashboard - Monitorring of jobs, transfers, IO rate, Tier 0.", link: "http://arda-dashboard.cern.ch/cms/", label: "Dashboard", id: "dashboard"}, 
 	{title: "DBS/DLS Discovery - Data set book keeping and location.", link: "https://cmsweb.cern.ch/dbs_discovery/", label: "DBS Discovery", id:"dbs"},
 	{title: "ProdRequest - Request large scale official production of samples.", link: "https://cmsweb.cern.ch/prodrequest/", label: "ProdRequest", id:"prodrequest"},
-	/*{title: "PhEDEx - Data placement, transfer monitoring", link: "http://cmsweb.cern.ch/phedex/", label: "PhEDEx", id:"phedex"},*/
-        {title: "Data placement, transfer, monitoring", link: "http://cmsweb.cern.ch/Common/datatransfer", label: "DataTransfer", id:"phedex"},
+	{title: "PhEDEx - Data placement, transfer monitoring", link: "http://cmsweb.cern.ch/phedex/", label: "PhEDEx", id:"phedex"},
+    //{title: "Data placement, transfer, monitoring", link: "http://cmsweb.cern.ch/Common/datatransfer", label: "DataTransfer", id:"phedex"},
 	{title: "SiteDB - Site information and aggregate monitoring", link: "https://cmsweb.cern.ch/sitedb/sitelist/", label: "SiteDB", id: "sitedb"},
 	{title: "CondDB - Conditions Database", link: "https://cmsweb.cern.ch/conddb/", label: "CondDB", id: "conddb"},
 	{title: "Web Tools Support - File a bug report, ask for help, read our FAQ", link: "https://cmsweb.cern.ch/sitedb/Common/help", label: "Support", id: "help"}]
