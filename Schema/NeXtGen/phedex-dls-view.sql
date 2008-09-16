855132367b9c20387a84370d3b4d53bde951f43b
afaq <>
2008-09-16 21:28:30 +0000

    View of View for sharing info with Phedex

---
+++ Schema/NeXtGen/phedex-dls-view.sql
@@ -0,0 +1,10 @@
+REATE OR REPLACE FORCE VIEW CMS_DBS_TEST_GLOBAL.SEBlock (SEID, BLOCKID) AS SELECT distinct se.ID as SEID, b.ID as BlockID from CMS_DBS_TEST_GLOBAL.StorageElement se, cms_transfermgmt_testbed.v_dbs_block_replica blkreplica, CMS_DBS_TEST_GLOBAL.Block b where b.Name=blkreplica.block_name AND se.SENAME=blkreplica.se_name;
+
+
+GRANT SELECT ON CMS_DBS_TEST_GLOBAL.SEBlock TO CMS_DBS_TEST_GLOBAL_ADMIN;
+GRANT SELECT ON CMS_DBS_TEST_GLOBAL.SEBlock TO CMS_DBS_TEST_GLOBAL_WRITER;
+GRANT SELECT ON CMS_DBS_TEST_GLOBAL.SEBlock TO CMS_DBS_TEST_GLOBAL_READER;
+
+
+select count(*) from CMS_DBS_TEST_GLOBAL.SEBlock;
+
