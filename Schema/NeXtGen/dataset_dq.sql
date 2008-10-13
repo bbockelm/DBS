513e755e36630476ab1c0177ce5a63b7fbce2b86
afaq <>
2008-10-13 23:58:20 +0000

    some mods during deplouyment

--- Schema/NeXtGen/dataset_dq.sql
+++ Schema/NeXtGen/dataset_dq.sql
@@ -1,4 +1,6 @@
-ALTER TABLE RunLumiQuality ADD ( Dataset integer  not null);
+--ALTER TABLE RunLumiQuality ADD ( Dataset integer  not null);
+alter table RunLumiQuality add Dataset int check (Dataset is not null);
+:wq
 
 ALTER TABLE RunLumiQuality ADD CONSTRAINT
     RunLumiQuality_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
@@ -27,5 +29,67 @@ ALTER TABLE IntQualityHistory ADD ( Dataset integer  not null);
 ALTER TABLE IntQualityHistory ADD CONSTRAINT
     IntQualityHistory_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
 /
+
+
+
+select * from RunLumiQuality;
+
+ALTER TABLE RunLumiQuality ADD ( Dataset integer);
+alter table RunLumiQuality add Dataset int check (Dataset is not null);
+
+
+alter table RunLumiQuality modify (Dataset integer);
+desc RunLumiQuality;
+alter table RunLumiQuality drop column dataset;
+
+ALTER TABLE RunLumiQuality ADD CONSTRAINT
+    RunLumiQuality_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
+/
+
+--
+alter table RunLumiDQInt add Dataset int check (Dataset is not null);
+ALTER TABLE RunLumiDQInt ADD ( Dataset integer not null);
+desc RunLumiDQInt;
+ALTER TABLE RunLumiDQInt ADD CONSTRAINT
+    RunLumiDQInt_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
+/
+
+--
+alter table QualityHistory add Dataset int check (Dataset is not null);
+ALTER TABLE QualityHistory ADD ( Dataset integer not null);
+Desc QualityHistory;
+ALTER TABLE QualityHistory ADD CONSTRAINT
+    QualityHistory_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
+/
+
+--
+desc IntQualityHistory;
+ALTER TABLE IntQualityHistory ADD ( Dataset integer  not null);
+alter table IntQualityHistory add Dataset int check (Dataset is not null);
+ALTER TABLE IntQualityHistory ADD CONSTRAINT
+    IntQualityHistory_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID)
+/
+
+alter table IntQualityHistory drop column dataset;
+
+
+
+select * from SchemaVersion;
+
+update cms_dbs_prod_global.SchemaVersion set schemaversion='DBS_1_1_3';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
