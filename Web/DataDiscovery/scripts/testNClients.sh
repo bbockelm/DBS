24fad2a06e7a0d9197aa34cbc2e303d54f30ffff
valya <>
2008-11-14 16:58:42 +0000

    Modify stress test to do against provided DD instance

--- Web/DataDiscovery/scripts/testNClients.sh
+++ Web/DataDiscovery/scripts/testNClients.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-   echo "\nUsage: testNClients.sh N, where N is a number of clients\n"
+if [ $# -eq 0 ]; then
+   echo -e "Usage: testNClients.sh N <prod|test>, where N is a number of clients"
    exit
 fi
 nClients=$1
+if [ "${2}" == "testbed" ]; then
+   if [ -z ${3} ]; then
+      echo -e "If testbed is used you MUST provide its URL"
+      exit
+   fi
+   host=${3}
+elif [ "${2}" == "prod" ]; then
+   host=https://cmsweb.cern.ch/dbs_discovery
+else
+   host=https://cmsweb.cern.ch/dbs_discovery_$2
+fi
+echo -e "Using host: $host"
 
 COUNTER=0
 rm -f testNClients.log
+touch testNClients.log
 
 while [  $COUNTER -lt $nClients ]; do
  echo Request $COUNTER
- $DDHOME/dd.py --input="find run where dataset like *" \
-               --host=https://cmsweb.cern.ch/dbs_discovery_test/ \
-               --iface=dbsapi --xml | \
+ $DDHOME/tools/DDSearchCLI.py \
+               --input="find site where site like *" \
+               --host=${host} \
+               --iface=dbsapi \
+        2>&1 1>> testNClients.log < /dev/null &
+ $DDHOME/tools/DDSearchCLI.py \
+               --input="find site where site like *" \
+               --host=${host} \
+               --iface=dbsapi \
+               --dbsInst=cms_dbs_ph_analysis_01 \
+        2>&1 1>> testNClients.log < /dev/null &
+ $DDHOME/tools/DDSearchCLI.py \
+               --input="find site where site like *" \
+               --host=${host} \
+               --iface=dbsapi \
+               --dbsInst=cms_dbs_caf_analysis_01 \
         2>&1 1>> testNClients.log < /dev/null &
  let COUNTER=COUNTER+1 
 done
