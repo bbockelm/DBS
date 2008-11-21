b5eebae3e69a202d3aab5fb254ae13ff39339faa
afaq <>
2008-11-21 22:21:30 +0000

    sanity check

--- Schema/NeXtGen/DBS-NeXtGen-Oracle_DEPLOYABLE.sql
+++ Schema/NeXtGen/DBS-NeXtGen-Oracle_DEPLOYABLE.sql
@@ -785,6 +785,21 @@ CREATE TABLE Block
 
 REM ======================================================================
 
+CREATE TABLE BlockParent
+  ( 
+    ID                    integer,
+    ThisBlock             integer   not null,
+    ItsParent             integer   not null,
+    CreatedBy             integer,
+    CreationDate          integer,
+    LastModifiedBy        integer,
+    LastModificationDate  integer,
+    primary key(ID),
+    unique(ThisBlock,ItsParent)
+  );
+
+REM ======================================================================
+
 CREATE TABLE Files
   (
     ID                    integer,
@@ -1470,6 +1485,19 @@ ALTER TABLE Block ADD CONSTRAINT
     Block_LastModifiedBy_FK foreign key(LastModifiedBy) references Person(ID)
 /
 
+ALTER TABLE BlockParent ADD CONSTRAINT
+    BlockParent_ThisBlock_FK foreign key(ThisBlock) references Block(ID) on delete CASCADE
+/
+ALTER TABLE BlockParent ADD CONSTRAINT
+    BlockParent_ItsParent_FK foreign key(ItsParent) references Block(ID) on delete CASCADE
+/
+ALTER TABLE BlockParent ADD CONSTRAINT
+    BlockParent_CreatedBy_FK foreign key(CreatedBy) references Person(ID)
+/
+ALTER TABLE BlockParent ADD CONSTRAINT
+    BlockParentLastModifiedBy_FK foreign key(LastModifiedBy) references Person(ID)
+/
+
 ALTER TABLE Files ADD CONSTRAINT 
     Files_Dataset_FK foreign key(Dataset) references ProcessedDataset(ID) on delete CASCADE
 /
@@ -1729,6 +1757,8 @@ PROMPT creating sequence seq_filetier ;
 create sequence seq_filetier ;
 PROMPT creating sequence seq_fileparentage ;
 create sequence seq_fileparentage ;
+PROMPT creating sequence seq_blockparent ;
+create sequence seq_blockparent ;
 PROMPT creating sequence seq_filerunlumi ;
 create sequence seq_filerunlumi ;
 PROMPT creating sequence seq_filealgo ;
@@ -2123,6 +2153,14 @@ PROMPT AUTO INC TRIGGER FOR Trigger for Table: filetier
 PROMPT AUTO INC TRIGGER FOR Trigger for Table: fileparentage
  CREATE OR REPLACE TRIGGER fileparentage_TRIG before insert on fileparentage    for each row begin     if inserting then       if :NEW.ID is null then          select seq_fileparentage.nextval into :NEW.ID from dual;       end if;    end if; end;
 /
+
+-- ====================================================
+-- AUTO INC TRIGGER FOR blockparent.ID using SEQ seq_blockparent
+
+PROMPT AUTO INC TRIGGER FOR Trigger for Table: blockparent
+ CREATE OR REPLACE TRIGGER blockparent_TRIG before insert on blockparent    for each row begin     if inserting then       if :NEW.ID is null then          select seq_blockparent.nextval into :NEW.ID from dual;       end if;    end if; end;
+/
+
 
 -- ====================================================
 -- AUTO INC TRIGGER FOR filerunlumi.ID using SEQ seq_filerunlumi
@@ -2852,6 +2890,21 @@ BEGIN
   :NEW.LASTMODIFICATIONDATE := unixtime;
 END;
 /
+
+-- ====================================================
+-- LastModified Time Stamp Trigger
+
+PROMPT LastModified Time Stamp Trigger for Table: blockparent
+CREATE OR REPLACE TRIGGER TRTSblockparent BEFORE INSERT OR UPDATE ON BlockParent
+FOR EACH ROW declare
+  unixtime integer
+     :=  (86400 * (sysdate - to_date('01/01/1970 00:00:00', 'DD/MM/YYYY HH24:MI:SS'))) - (to_number(substr(tz_offset(sessiontimezone),1,3))) * 3600 ;
+BEGIN
+  :NEW.LASTMODIFICATIONDATE := unixtime;
+END;
+/
+
+
 
 -- ====================================================
 -- LastModified Time Stamp Trigger
