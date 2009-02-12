a5b56bd5911aa0d1c7272528218899e36bd748e1
valya <>
2009-02-12 19:40:32 +0000

    Script which generate DDL for DBS views with ORACLE and MySQL DB back-ends

---
+++ Schema/NeXtGen/buildViews.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Create views for ORACLE instances"
+for dbs in "CMS_DBS_PROD_GLOBAL" "CMS_DBS_PROD_LOCAL_01"
+do
+    cat view_templates.sql \
+    | sed "s/@build.schema.owner.name@/$dbs/" > oracle_views_$dbs.sql
+done
+
+echo "Create views for MySQL"
+cat view_templates.sql | grep -v -i prompt \
+| perl -0777 -ple 's!(_READER)\n/!$1!sg' \
+| grep -v -i grant | sed "s/\//;/g" \
+| sed "s/Drop view/drop view if exists/g" > mysql_views.sql
