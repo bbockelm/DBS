fab9086e41d261dda0b83845a59a47c263b5879c
liangd <>
2009-05-23 01:07:49 +0000

    New dbs UnitTests

--- Clients/Python/DBSAPI/UnitTests/dbsUnitTests.py
+++ Clients/Python/DBSAPI/UnitTests/dbsUnitTests.py
@@ -3,15 +3,18 @@ import validate
 #import ValidateMigration
 import dbsAPIUnitTests
 import dbsQLUnitTests
+import dbsViewsUnitTests
 
 suite1 = dbsAPIUnitTests.suite()
 suite2 = dbsQLUnitTests.fillsuite()
 suite3 = validate.suite()
+suite5 = dbsViewsUnitTests.suite()
 #suite4 = ValidateMigration.suite()
 Suite  = unittest.TestSuite()
 Suite.addTest(suite1)
 Suite.addTest(suite2)
 Suite.addTest(suite3)
+Suite.addTest(suite5)
 #suite.addTest(suite4)
 unittest.TextTestRunner(verbosity=2).run(Suite)
 
