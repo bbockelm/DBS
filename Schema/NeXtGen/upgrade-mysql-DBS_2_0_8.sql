417453882cdc1e27c0ee0bb9a5f0a21321d4480f
yuyi <>
2009-07-20 19:43:36 +0000

    add dbs_release_version

--- Schema/NeXtGen/upgrade-mysql-DBS_2_0_8.sql
+++ Schema/NeXtGen/upgrade-mysql-DBS_2_0_8.sql
@@ -1,3 +1,6 @@
 use CMS_DBS;
 alter table ProcessedDataset add DESCRIPTION varchar(1000);
 update SchemaVersion set SchemaVersion='DBS_1_1_6';
+alter table SchemaVersion add DBS_RELEASE_VERSION varchar(100);
+update SchemaVersion set DBS_RELEASE_VERSION='DBS_2_0_8';
+commit;
--- Schema/NeXtGen/upgrade-oracle-DBS_2_0_8.sql
+++ Schema/NeXtGen/upgrade-oracle-DBS_2_0_8.sql
@@ -1,2 +1,5 @@
 alter table ProcessedDataset add (DESCRIPTION varchar2(1000));
 update SCHEMAVERSION set SCHEMAVERSION='DBS_1_1_6';
+alter table SchemaVersion add DBS_RELEASE_VERSION varchar2(100);
+update SchemaVersion set DBS_RELEASE_VERSION='DBS_2_0_8';
+commit;
