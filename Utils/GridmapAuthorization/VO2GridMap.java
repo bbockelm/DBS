126eaa2e905b16706a67bdcf428c97c36d066ebf
afaq <>
2009-07-28 17:55:57 +0000

    Comma seperated list of VOMS Groups can be provided on commandline

--- Utils/GridmapAuthorization/VO2GridMap.java
+++ Utils/GridmapAuthorization/VO2GridMap.java
@@ -4,33 +4,51 @@ import java.io.RandomAccessFile;
 import org.glite.security.voms.service.User;
 import org.glite.security.voms.service.admin.VOMSAdmin;
 import org.glite.security.voms.service.admin.VOMSAdminServiceLocator;
+import java.lang.Exception;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 
 public class VO2GridMap {
 	
 
 	private String defaultURL = "https://voms.cern.ch:8443/voms/cms/services/VOMSAdmin";
-	private String defaultGroup = "/cms/uscms/Role=production";
-	private String map = "sekhri";
+	//AA-07/28/2009 : No default role is required
+	//private String defaultGroup = "/cms/uscms/Role=production";
+	private String map = "cmsdbs";
 
 	public VO2GridMap (){}
 
-	
-
-	public void makeGridMap(String fileName, String url, String group) {
+	public void makeGridMap(String fileName, String url, String groups) {
 		try{	
 			if (url == null) url = defaultURL;
 			if (url.length() == 0) url = defaultURL;
-			if (group == null) group = defaultGroup;
-			if (group.length() == 0) group = defaultGroup;
+			if (groups == null) { 
+					System.out.println("No VOMS group specified");
+					throw new Exception("No VOMS group specified");
+			}
 			System.out.println("URL is "+ url);
 
 			VOMSAdmin stub = makeStub(url);
-			User users[]= stub.listMembers(group);
+			ArrayList<String> allDN = new ArrayList<String>();
+			StringTokenizer st = new StringTokenizer(groups, ",");
+                        while (st.hasMoreTokens()) {
+				String group=st.nextToken();
+                                System.out.println("Now Procssing VOMS Group :"+group);
+				User users[]= stub.listMembers(group);
+				for(int i=0;i<users.length;i++) {
+					String dn = users[i].getDN();
+					if (!allDN.contains(dn)) {
+						allDN.add(dn);		
+						System.out.println(dn);
+					}
+				}
+                        }
+
 			RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
-			for(int i=0;i<users.length;i++) {
-				System.out.println("\"" + users[i].getDN() + "\" " + map);
-				raf.writeBytes("\"" + users[i].getDN() + "\" " + map + "\n");
+			for(int i=0;i != allDN.size(); ++i) {
+				System.out.println("\"" + allDN.get(i) + "\" " + map);
+				raf.writeBytes("\"" + allDN.get(i) + "\" " + map + "\n");
 			}
 			raf.close();
 				
@@ -52,10 +70,11 @@ public class VO2GridMap {
 		return locator.getVOMSAdmin(new URL(location));
 	}
 
-	
-	
-
 	public static void main(String args[]){
-		(new VO2GridMap()).makeGridMap(args[0], args[1], args[2]);
+		if (args.length < 3) {
+			System.out.println("No VOMS group specified, specify at least one voms group");
+			System.out.println("USAGE: $JAVA_HOME/bin/java -classpath $CLASSPATH -Daxis.socketSecureFactory=org.glite.security.trustmanager.axis.AXISSocketFactory -DsslConfigFile=$AF -Daxis.ClientConfigFile=client-config.wsdd VO2GridMap <outputfile> <url> <group1,group2,group3...>");
+		} else
+			(new VO2GridMap()).makeGridMap(args[0], args[1], args[2]);
 	}
 }
