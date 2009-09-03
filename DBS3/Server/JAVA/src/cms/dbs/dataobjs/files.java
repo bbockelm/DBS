91b1f74ad12720c45b2548eeddd45eade4af7251
afaq <>
2009-09-03 21:22:37 +0000

    adding dataobject files

---
+++ DBS3/Server/JAVA/src/cms/dbs/dataobjs/files.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ $Revision: $"
+ $Id: $"
+ *
+ * Data Object from table : FILES
+*/
+
+package cms.dbs.dataobjs;
+
+public class files extends JSONObject {
+
+
+}
---
+++ DBS3/Server/JAVA/src/cms/dbs/dataobjs/paths.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ $Revision: $"
+ $Id: $"
+ *
+ * Data Object from table : PATHS
+*/
+
+package cms.dbs.dataobjs;
+
+public class paths extends JSONObject {
+
+
+}
