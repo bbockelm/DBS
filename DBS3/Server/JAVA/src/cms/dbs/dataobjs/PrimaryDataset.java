66ceb14ad9f14a86bd660f71d0055cb8eafe1da0
yuyi <>
2009-09-10 13:21:53 +0000

    change type id to type dataobject and add exceptions

--- DBS3/Server/JAVA/src/cms/dbs/dataobjs/PrimaryDataset.java
+++ DBS3/Server/JAVA/src/cms/dbs/dataobjs/PrimaryDataset.java
@@ -1,12 +1,16 @@
 /**
  * 
  $Revision: 1.2 $"
- $Id: generate_dataobjs.py,v 1.2 2009/09/04 20:21:16 afaq Exp $"
+ $Id: PrimaryDataset.java,v 1.2 2009/09/04 20:24:48 afaq Exp $"
  *
  * Data Object from table : PRIMARY_DATASETS
 */
 
 package cms.dbs.dataobjs;
+
+import cms.dbs.dataobjs.PrimaryDSType;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class PrimaryDataset extends JSONObject {
 
@@ -14,16 +18,16 @@ public class PrimaryDataset extends JSONObject {
 
 	}
 
-        public PrimaryDataset ( int primaryDSID, String primaryDSName, int primaryDSTypeID, int creationDate, String createBy )  {
+        public PrimaryDataset ( int primaryDSID, String primaryDSName, PrimaryDSType pType, int creationDate, String createBy )  {
 		
                 this.putOnce("PRIMARY_DS_ID", (Integer) primaryDSID );
                 this.putOnce("PRIMARY_DS_NAME", (String) primaryDSName );
-                this.putOnce("PRIMARY_DS_TYPE_ID", (Integer) primaryDSTypeID );
+                this.putOnce("PRIMARY_DS_TYPE_DO", pType );
                 this.putOnce("CREATION_DATE", (Integer) creationDate );
                 this.putOnce("CREATE_BY", (String) createBy );
         }
 
-	int getPrimaryDSID ( ) {
+	int getPrimaryDSID ( ) throws Exception{
 		int primaryDSID = null;
                	if (!JSONObject.NULL.equals(this.get("PRIMARY_DS_ID"))) {
                        	primaryDSID = (Integer) this.get("PRIMARY_DS_ID");
@@ -31,7 +35,7 @@ public class PrimaryDataset extends JSONObject {
                 return primaryDSID;
         }
 	
-	String getPrimaryDSName ( ) {
+	String getPrimaryDSName ( ) throws Exception{
 		String primaryDSName = null;
                	if (!JSONObject.NULL.equals(this.get("PRIMARY_DS_NAME"))) {
                        	primaryDSName = (String) this.get("PRIMARY_DS_NAME");
@@ -39,15 +43,15 @@ public class PrimaryDataset extends JSONObject {
                 return primaryDSName;
         }
 	
-	int getPrimaryDSTypeID ( ) {
-		int primaryDSTypeID = null;
-               	if (!JSONObject.NULL.equals(this.get("PRIMARY_DS_TYPE_ID"))) {
-                       	primaryDSTypeID = (Integer) this.get("PRIMARY_DS_TYPE_ID");
+	int getPrimaryDSTypeDO ( ) throws Exception{
+		JSONObject primaryDSType = null;
+               	if (! this.isNull("PRIMARY_DS_TYPE_DO")) {
+                       	primaryDSType =  this.getJSONObject("PRIMARY_DS_TYPE_DO");
                	}
-                return primaryDSTypeID;
+                return primaryDSType;
         }
 	
-	int getCreationDate ( ) {
+	int getCreationDate ( ) throws Exception{
 		int creationDate = null;
                	if (!JSONObject.NULL.equals(this.get("CREATION_DATE"))) {
                        	creationDate = (Integer) this.get("CREATION_DATE");
@@ -55,7 +59,7 @@ public class PrimaryDataset extends JSONObject {
                 return creationDate;
         }
 	
-	String getCreateBy ( ) {
+	String getCreateBy ( ) throws Exception{
 		String createBy = null;
                	if (!JSONObject.NULL.equals(this.get("CREATE_BY"))) {
                        	createBy = (String) this.get("CREATE_BY");
