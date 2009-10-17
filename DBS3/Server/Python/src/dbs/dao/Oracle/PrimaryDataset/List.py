2cd7a32fabcc693fde3d3220e7fb0d7c5ae3b8d6
akhukhun <>
2009-10-17 18:03:29 +0000

    adding schemaowner

--- DBS3/Server/Python/src/dbs/dao/Oracle/PrimaryDataset/List.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/PrimaryDataset/List.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This module provides PrimaryDSType.List data access object.
+This module provides PrimaryDataset.List data access object.
 """
 __revision__ = "$Id: $"
 __version__ = "$Revision: =$"
@@ -9,7 +9,7 @@ __version__ = "$Revision: =$"
 from WMCore.Database.DBFormatter import DBFormatter
 class List(DBFormatter):
     """
-    PrimaryDSType List DAO class.
+    PrimaryDataset List DAO class.
     """
     def __init__(self, logger, dbi):
         """
@@ -19,19 +19,25 @@ class List(DBFormatter):
         self.owner = "%s." % self.dbi.engine.url.username
         self.sql = \
 """
-SELECT PT.PRIMARY_DS_TYPE_ID, PT.PRIMARY_DS_TYPE
-FROM %sPRIMARY_DS_TYPES PT 
-""" % (self.owner)
+SELECT P.PRIMARY_DS_ID, P.PRIMARY_DS_NAME, P.PRIMARY_DS_TYPE_ID,
+    PT.PRIMARY_DS_TYPE, P.CREATION_DATE, P.CREATE_BY
+FROM %sPRIMARY_DATASETS P
+JOIN %sPRIMARY_DS_TYPES PT
+ON PT.PRIMARY_DS_TYPE_ID=P.PRIMARY_DS_TYPE_ID
+""" % (self.owner, self.owner)
 
     def execute(self, pattern = "", conn = None, transaction = False):
         """
-        Lists all primary dataset types if pattern is not provided.
+        Lists all primary datasets if pattern is not provided.
         """
         sql = self.sql
         if pattern == "":
             result = self.dbi.processData(sql, conn=conn, transaction=transaction)
         else:
-            sql += "WHERE PT.PRIMARY_DS_TYPE = :primdstype" 
-            binds = {"primdstype":pattern}
+            if  pattern.find("%")==-1:
+                sql += "WHERE P.PRIMARY_DS_NAME = :primdsname"
+            else:
+                sql += "WHERE P.PRIMARY_DS_NAME like  :primdsname"
+            binds = {"primdsname":pattern}
             result = self.dbi.processData(sql, binds, conn, transaction)
         return self.formatDict(result)
