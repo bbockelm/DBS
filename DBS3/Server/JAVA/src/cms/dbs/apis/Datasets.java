1349ec85d3ae1abcb2c77f3557e615d075bf5557
afaq <>
2009-10-26 11:33:21 +0000

    Untested code checkin for Dataset REST interface

--- DBS3/Server/JAVA/src/cms/dbs/apis/Datasets.java
+++ DBS3/Server/JAVA/src/cms/dbs/apis/Datasets.java
@@ -1,5 +1,5 @@
 /***
- * $Id: Datasets.java,v 1.4 2009/10/21 17:51:42 afaq Exp $
+ * $Id: Datasets.java,v 1.1 2009/10/26 11:04:55 afaq Exp $
  * DBS Server side APIs .
  ***/
 
@@ -23,13 +23,13 @@ import cms.dbs.apis.DBSApis;
 
 public class Datasets extends Resource {
 
-    String primaryDatasetName;
+    String dataset;
 
     public Datasets(Context context, Request request, Response response) {
 	
         super(context, request, response);
 
-	this.primaryDatasetName = (String)request.getAttributes().get("PRIMARY_DS_NAME");
+	this.dataset = (String)request.getAttributes().get("DATASET");
 
          // Allow modifications of this resource via POST/PUT/DELETE requests.  
          setModifiable(true);
@@ -76,6 +76,22 @@ public class Datasets extends Resource {
         return representation;
     }
 
+
+
+
+
+
+	    ProcessedDataset processedDS = new  ProcessedDataset(0, "ProcessedDS-Yuyi4");
+            Dataset ds = new Dataset(0, "/TEST10-Primary/ProcessedDS-Yuyi4/GEN-SIM-RAW", 1, PD, processedDS, new DataTier(0, "GEN-SIM-RAW"),
+                        new DatasetType(0, "PRODUCTION"), null, null, new PhysicsGroup(6, "QCD"), 0.01, "Yuyi's Tag", 0, "");
+            System.out.println(api.DBSApiInsertDataset(ds));
+
+
+
+
+
+
+
     //AA -- POST
     /** 
      * Handle POST requests: create a new item. (insert primrat datasets)
@@ -88,19 +104,17 @@ public class Datasets extends Resource {
 		//Seems like you can only read ONCE from the entity (is it a stream?)
                 JSONObject json_req = new JSONObject(entity.getText());
 		//System.out.println("json_req:::"+json_req);
-		//Incoming object has BOTH type and name of Primary dataset, 
-		//{"PRIMARY_DS_TYPE":"test","PRIMARY_DS_NAME":"TEST9"}
-           	PrimaryDSType PT = new PrimaryDSType(0, json_req.getString("PRIMARY_DS_TYPE"));
-
 		/*  We should put some checks in here
 		String primaryDSName = null;
                 if (!JSONObject.NULL.equals(json_req.getString("PRIMARY_DS_NAME"))) {
                         primaryDSName =  json_req.getString("PRIMARY_DS_NAME");
                 }*/
-            	PrimaryDataset PD = new PrimaryDataset(0, json_req.getString("PRIMARY_DS_NAME"), PT, 0, "");
 
+		//FIXME: An ugly contraption od Dataset object, we should have something cleaner for better error handlingg !!!
+		Dataset ds = new Dataset(0, json_req.getString("DATASET"), 1, PD, new ProcessedDataset(0, json_req.getString("PROCESSSED_DATASED_NAME")), new DataTier(0, json_req.getString("DATA_TIER")), new DatasetType(0, json_req.getString("DATASET_TYPE"), null, null, new PhysicsGroup(0, json_req.getString("PHYSICS_GROUP"), json_req.getString("???", json_req.getString("????"), 0, json_req.getString("????") );
+		
 		DBSApis api = new DBSApis();	
-            	api.DBSApiInsertPrimaryDataset((PrimaryDataset)PD);
+            	api.DBSApiInsertDataset(DS);
 
         } catch(Exception ex){
             	System.out.println("Exception raised :" + ex.getMessage() + ". " );
