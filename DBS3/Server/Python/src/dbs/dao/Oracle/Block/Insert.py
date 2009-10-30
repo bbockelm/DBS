29f540b33fdfbfd7dcd53ad2d6a03438f1776850
akhukhun <>
2009-10-30 16:47:39 +0000

    some minor changes

--- DBS3/Server/Python/src/dbs/dao/Oracle/Block/Insert.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/Block/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for Blocks table """ 
 
-__revision__ = "$Revision: 1.1.1.1 $"
-__version__  = "$Id: Insert.py,v 1.1.1.1 2009/10/22 15:33:55 akhukhun Exp $ "
+__revision__ = "$Revision: 1.3 $"
+__version__  = "$Id: Insert.py,v 1.3 2009/10/27 17:24:48 akhukhun Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -18,13 +18,13 @@ class Insert(DBFormatter):
 INSERT INTO %sBLOCKS 
 (BLOCK_ID, BLOCK_NAME, DATASET_ID, OPEN_FOR_WRITING, ORIGIN_SITE, BLOCK_SIZE, 
 FILE_COUNT, CREATION_DATE, CREATE_BY, LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
-VALUES(:blockid, :blockname, :datasetid, :openforwriting, :originsite, :blocksize, 
+VALUES(:blockid, :blockname, :dataset, :openforwriting, :originsite, :blocksize, 
 :filecount, :creationdate, :createby, :lastmodificationdate, :lastmodifiedby)
 """ % (self.owner)
 
     def execute( self, daoinput, conn = None, transaction = False):
         """daoinput must be validated to have the following keys:
-        blockid, blockname, datasetid, openforwriting, originsite, blocksize,
+        blockid, blockname, dataset(id), openforwriting, originsite(id), blocksize,
         filecount, creationdate, createby, lastmodificationdate, lastmodifiedby
         """
         self.dbi.processData(self.sql, daoinput, conn, transaction)
