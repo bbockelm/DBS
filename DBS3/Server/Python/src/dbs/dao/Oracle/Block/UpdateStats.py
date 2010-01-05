55f67fda8f107acfe9955d49f4fdcfc41d52158e
afaq <>
2010-01-05 00:24:59 +0000

    Check pointing: after the next work failure, insert file changes

--- DBS3/Server/Python/src/dbs/dao/Oracle/Block/UpdateStats.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/Block/UpdateStats.py
@@ -2,8 +2,8 @@
 """
 This module provides Block.UpdateStats data access object.
 """
-__revision__ = "$Id: i$"
-__version__ = "$Revision: $"
+__revision__ = "$Id: UpdateStats.py,v 1.1 2010/01/04 21:02:19 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 class UpdateStats(DBFormatter):
@@ -16,11 +16,11 @@ class UpdateStats(DBFormatter):
         """
         DBFormatter.__init__(self, logger, dbi)
         self.owner = "%s." % owner
-        self.sql = """UPDATE %sBLOCK SET FILE_COUNT=:file_count, block_size=:block_size where BLOCK_ID=:block_id""" %  self.owner 
+        self.sql = """UPDATE %sBLOCKS SET FILE_COUNT=:file_count, block_size=:block_size where BLOCK_ID=:block_id""" %  self.owner 
         
     def execute(self, blockStats, conn = None, transaction = False):
         """
         for a given block_id
         """	
 	print self.sql
-        result = self.dbi.processData(sql, blockStats, conn, transaction)
+        result = self.dbi.processData(self.sql, blockStats, conn, transaction)
