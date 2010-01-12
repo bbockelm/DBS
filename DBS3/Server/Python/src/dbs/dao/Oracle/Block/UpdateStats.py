c005e2ba48e9b6065e57cee1c1ab3350bad4ba88
afaq <>
2010-01-12 17:38:00 +0000

    fixed some DAOs to use local sql var instead of class self.sql, this makes DAO object reusable in a loop

--- DBS3/Server/Python/src/dbs/dao/Oracle/Block/UpdateStats.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/Block/UpdateStats.py
@@ -2,8 +2,8 @@
 """
 This module provides Block.UpdateStats data access object.
 """
-__revision__ = "$Id: UpdateStats.py,v 1.1 2010/01/04 21:02:19 afaq Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateStats.py,v 1.2 2010/01/05 00:24:57 afaq Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 class UpdateStats(DBFormatter):
@@ -22,5 +22,4 @@ class UpdateStats(DBFormatter):
         """
         for a given block_id
         """	
-	print self.sql
         result = self.dbi.processData(self.sql, blockStats, conn, transaction)
