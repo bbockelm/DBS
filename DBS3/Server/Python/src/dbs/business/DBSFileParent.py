e93e5c57e1b8b7223f6c8aad379e025a74e2f84a
afaq <>
2010-01-26 22:17:38 +0000

    exception should be raised if lfn is not provided

--- DBS3/Server/Python/src/dbs/business/DBSFileParent.py
+++ DBS3/Server/Python/src/dbs/business/DBSFileParent.py
@@ -3,8 +3,8 @@
 This module provides business object class to interact with FileParent. 
 """
 
-__revision__ = "$Id: DBSDataset.py,v 1.13 2009/12/22 21:50:14 afaq Exp $"
-__version__ = "$Revision: 1.13 $"
+__revision__ = "$Id: DBSFileParent.py,v 1.1 2010/01/01 19:02:40 akhukhun Exp $"
+__version__ = "$Revision: 1.1 $"
 
 from WMCore.DAOFactory import DAOFactory
 
@@ -28,4 +28,6 @@ class DBSFileParent:
         required parameter: logical_file_name
         returns: logical_file_name, parent_logical_file_name, parent_file_id
         """
+	if not logical_file_name:
+	    raise Exception("logical_file_name is required for listFileParents api")
         return self.fileparentlist.execute(logical_file_name)
