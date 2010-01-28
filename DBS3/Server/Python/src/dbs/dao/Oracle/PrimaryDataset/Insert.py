a863315c772c4bd1be6b6859a29f343dc3701645
afaq <>
2010-01-28 22:54:02 +0000

    Fixed some conn and trans parameters to executeSingle call, they were defaulted, hence framework was managing the connections in that case, we do not wnat that, though it helped uncover a bug in wmcore

--- DBS3/Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for PrimaryDatasets table """ 
 
-__revision__ = "$Revision: 1.9 $"
-__version__  = "$Id: Insert.py,v 1.9 2009/12/17 17:51:28 yuyi Exp $ "
+__revision__ = "$Revision: 1.10 $"
+__version__  = "$Id: Insert.py,v 1.10 2009/12/23 15:42:05 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
@@ -12,6 +12,6 @@ class Insert(InsertSingle):
     """ PrimaryDataset Insert DAO Class"""
     def execute(self, daoinput, conn = None, transaction = False):
 	try:
-	    self.executeSingle(daoinput, "PRIMARY_DATASETS", conn = None, transaction = False)
+	    self.executeSingle(daoinput, "PRIMARY_DATASETS", conn , transaction )
 	except Exception:
 	    raise
--- DBS3/Server/Python/src/dbs/dao/Oracle/Site/Insert.py
+++ DBS3/Server/Python/src/dbs/dao/Oracle/Site/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for Sites table """ 
 
-__revision__ = "$Revision: 1.2 $"
-__version__  = "$Id: Insert.py,v 1.2 2009/10/20 02:19:23 afaq Exp $ "
+__revision__ = "$Revision: 1.3 $"
+__version__  = "$Id: Insert.py,v 1.3 2009/12/23 20:45:47 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
@@ -12,7 +12,7 @@ class Insert(InsertSingle):
     """ DAO for Insert Site """
     def execute( self, daoinput, conn=None, transaction=False ):
 	try:
-            self.executeSingle(daoinput, "SITES", conn = None, transaction = False)
+            self.executeSingle(daoinput, "SITES", conn, transaction)
         except Exception:
             raise
 
