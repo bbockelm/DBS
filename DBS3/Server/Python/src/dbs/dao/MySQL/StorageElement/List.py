fdf954869ec0d1e2eb453ef6a1ee31801bb94611
afaq <>
2010-03-02 20:23:36 +0000

    mysql dao for storagelements

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/StorageElement/List.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides StorageElement.List data access object.
+"""
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+
+from dbs.dao.Oracle.StorageElement.List import List as OraStorageElementList
+
+class List(OraStorageElementList):
+        pass
+
---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/StorageElement/ListBlockSE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+This module provides StorageElement.ListBlockSE data access object.
+"""
+
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+
+from dbs.dao.Oracle.StorageElement.ListBlockSE import List as OraStorageElementListBlockSE
+
+class ListBlockSE(OraStorageElementListBlockSE):
+        pass
+
