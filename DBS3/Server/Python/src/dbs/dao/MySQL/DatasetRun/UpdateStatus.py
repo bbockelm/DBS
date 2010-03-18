34007b0f786636183453140c5079b61da93d3275
afaq <>
2010-03-18 14:41:09 +0000

    added updateDatasetStatus api

--- DBS3/Server/Python/src/dbs/dao/MySQL/DatasetRun/UpdateStatus.py
+++ DBS3/Server/Python/src/dbs/dao/MySQL/DatasetRun/UpdateStatus.py
@@ -2,10 +2,10 @@
 """
 This module provides Dataset.UpdateStatus data access object.
 """
-__revision__ = "$Id: UpdateStatus.py,v 1.1 2010/03/04 17:59:18 afaq Exp $"
+__revision__ = "$Id: UpdateStatus.py,v 1.1 2010/03/08 23:12:38 afaq Exp $"
 __version__ = "$Revision: 1.1 $"
 
-from dbs.dao.Oracle.Dataset.UpdateStatus import UpdateStatus as OraDatasetUpdateStatus
+from dbs.dao.Oracle.DatasetRun.UpdateStatus import UpdateStatus as OraDatasetRunUpdateStatus
 
-class UpdateStatus(OraDatasetUpdateStatus):
+class UpdateStatus(OraDatasetRunUpdateStatus):
     pass
