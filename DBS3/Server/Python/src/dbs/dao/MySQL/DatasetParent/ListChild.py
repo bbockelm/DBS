9c3c3a8ea29bb9a6909ce4fff00557d0dddc31a9
afaq <>
2010-04-20 20:08:11 +0000

    added list dataset chldren api

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/DatasetParent/ListChild.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides DatasetParent.ListChild data access object.
+"""
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+
+from dbs.dao.Oracle.DatasetParent.ListChild import ListChild as OraDatasetChildList
+
+class ListChild(OraDatasetChildList):
+        pass
+
