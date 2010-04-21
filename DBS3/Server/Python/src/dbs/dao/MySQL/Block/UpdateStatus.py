2d9faa2e03a7869d1b9e6a91ad51f722d8dca934
afaq <>
2010-04-21 19:42:36 +0000

    added missing UpdateStatus file

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/Block/UpdateStatus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+This module provides Block.UpdateStatus data access object.
+"""
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+
+from dbs.dao.Oracle.Block.UpdateStatus import UpdateStatus as OraBlockUpdateStatus
+
+class UpdateStatus(OraBlockUpdateStatus):
+    pass
