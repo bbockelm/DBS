257df4e0145e0465a2b56626e491f91eed0c29cb
afaq <>
2010-06-10 21:51:26 +0000

    component status reporting dao

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/ComponentStatus/Insert.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: $"
+__version__  = "$Id: $ "
+
+from dbs.dao.Oracle.ComponentStatus.Insert import Insert as OraComponentStatusInsert 
+
+class Insert(OraComponentStatusInsert):
+            pass
+	
---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/ComponentStatus/Update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: $"
+__version__  = "$Id: $ "
+
+from dbs.dao.Oracle.ComponentStatus.Update import Update as OraComponentStatusUpdate
+
+class Update(OraComponentStatusUpdate):
+            pass
+	
