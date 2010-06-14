f2ef4a5db7db9532b3c24d0f5c7b0e9872dc2434
afaq <>
2010-06-14 15:10:58 +0000

    dao to get status of dbs

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/ComponentStatus/DBSStatus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: $"
+__version__  = "$Id: $ "
+
+from dbs.dao.Oracle.ComponentStatus.DBSStatus import DBSStatus as OraDBSStatus
+
+class DBSStatus(OraDBSStatus):
+            pass
+	
