94fe4ba7395b5cea40e393dc4fd23807792f7c03
afaq <>
2010-06-14 14:34:34 +0000

    added the mysql dao for component status

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/ComponentStatus/List.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: $"
+__version__  = "$Id: $ "
+
+from dbs.dao.Oracle.ComponentStatus.List import List as OraComponentStatusList
+
+class List(OraComponentStatusList):
+            pass
+	
