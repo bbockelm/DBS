ae57539d43efc901a5f650ab94e0fc17041db3fb
yuyi <>
2010-08-18 18:48:59 +0000

    *** empty log message ***

--- DBS3/Server/Python/src/dbs/dao/MySQL/MigrationRequests/FindPendingRequest.py
+++ DBS3/Server/Python/src/dbs/dao/MySQL/MigrationRequests/FindPendingRequest.py
@@ -2,10 +2,10 @@
 """
 This module provides MigrationRequests.FindPendingRequest data access object.
 """
-__revision__ = "$Id:$"
-__version__ = "$Revision: $"
+__revision__ = "$Id: FindPendingRequest.py,v 1.1 2010/08/18 18:46:38 yuyi Exp $"
+__version__ = "$Revision: 1.1 $"
 
-from dbs.dao.Oracle.MigrationRequests.FindPendingRequest import List as OraMigFindPendingRequest
+from dbs.dao.Oracle.MigrationRequests.FindPendingRequest import FindPendingRequest as OraMigFindPendingRequest
 
 class FindPendingRequest(OraMigFindPendingRequest):
     pass
