584576fcad3ee1db607eeab400234043525dd9ee
yuyi <>
2010-08-18 19:01:43 +0000

    initial checking

---
+++ DBS3/Server/Python/src/dbs/dao/MySQL/MigrationBlock/FindMigrateableBlock.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides MigrationBlock.FindMigrateableBlock data access object.
+"""
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+
+from dbs.dao.Oracle.MigrationBlock.FindMigrateableBlock import FindMigrateableBlock as OraMigFindMigrateableBlock
+
+class FindMigrateableBlock(OraMigFindMigrateableBlock):
+    pass
+		   
