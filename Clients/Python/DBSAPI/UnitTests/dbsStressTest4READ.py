4e47d578610485e0503fc1270c7d828064643120
dsr <>
2012-08-21 15:01:05 +0000

    fix invalid octal number

--- Clients/Python/DBSAPI/UnitTests/dbsStressTest4READ.py
+++ Clients/Python/DBSAPI/UnitTests/dbsStressTest4READ.py
@@ -160,7 +160,7 @@ for i in range(maxDS):
                         LogicalFileName= lfn2,
                         #QueryableMetadata= 'This is a test file',
                         NumberOfEvents= 10000,
-                        FileSize= 0456778,
+                        FileSize= 456778,
                         Status= 'VALID',
                         FileType= 'EDM',
                         #LumiList= [lumi1, lumi2],
